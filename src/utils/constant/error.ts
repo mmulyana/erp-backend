ebf06153d93e4d8ea1b1c177b69736659a1bce2c
mmulyana <mulyan.t20@gmail.com>
2024-06-30 05:30:31 +0700

    feat(error): update error message

--- src/utils/constant/error.ts
+++ src/utils/constant/error.ts
@@ -1,3 +1,7 @@
 export const MESSAGE = {
-  ACCOUNT_ALREADY_USED: 'Email atau nama ini sudah digunakan, masukkan email atau nama baru',
+  ACCOUNT_ALREADY_USED:
+    'Email atau nama ini sudah digunakan, masukkan email atau nama baru',
+  GROUP_NAME_ALREADY_USED: 'Pilih nama grup baru',
+  CANT_DELETE_GROUP:
+    'Grup tidak bisa dihapus karena memiliki permission, silahkan hapus permission dahulu',
 }
