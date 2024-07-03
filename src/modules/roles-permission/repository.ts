7c03463e38138f76b648c543f7d2a26dfe074a5c
mmulyana <mulyan.t20@gmail.com>
2024-07-04 02:56:51 +0700

    feat(roles-permission): add repository

---
+++ src/modules/roles-permission/repository.ts
@@ -0,0 +1,94 @@
+import { RolesPermission } from '@prisma/client'
+import db from '../../lib/db'
+
+type Payload = {
+  rolesId: number
+  permissionId: number
+  enabled: boolean
+}
+
+interface IRolePermission {
+  create(payload: Payload): Promise<void>
+  update(payload: Payload, id: number): Promise<void>
+  delete(id: number): Promise<void>
+  read(id: number): Promise<RolesPermission | null>
+  readAll(): Promise<RolesPermission[]>
+}
+
+export default class RolePermissionRepository implements IRolePermission {
+  create = async (payload: Payload) => {
+    try {
+      await db.rolesPermission.create({
+        data: {
+          ...payload,
+        },
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+
+  update = async (payload: Payload, id: number) => {
+    try {
+      await db.rolesPermission.update({
+        data: {
+          ...payload,
+        },
+        where: {
+          id,
+        },
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+
+  read = async (id: number): Promise<RolesPermission | null> => {
+    try {
+      const data = await db.rolesPermission.findUnique({
+        where: {
+          id: Number(id),
+        },
+      })
+
+      if (!data) {
+        throw new Error('role permission not found')
+      }
+
+      return data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  readAll = async (): Promise<RolesPermission[]> => {
+    try {
+      const data = await db.rolesPermission.findMany()
+      return data || []
+    } catch (error) {
+      throw error
+    }
+  }
+
+  delete = async (id: number): Promise<void> => {
+    try {
+      const data = await db.rolesPermission.findUnique({
+        where: {
+          id: Number(id),
+        },
+      })
+
+      if (!data) {
+        throw new Error('role permission not found')
+      }
+
+      await db.rolesPermission.delete({
+        where: {
+          id,
+        },
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+}
