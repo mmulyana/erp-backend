ff26a927c4501bc1b3cbc7f9d50b482130e918d2
mmulyana <mulyan.t20@gmail.com>
2024-07-04 02:57:24 +0700

    feat(permission-group): update schema

--- src/modules/permission-group/schema.ts
+++ src/modules/permission-group/schema.ts
@@ -3,9 +3,13 @@ import { z } from 'zod'
 const permissionGroupSchema = {
   create: z.object({
     name: z.string(),
+    description: z.string().optional(),
+    permissionNames: z.string().array().optional(),
   }),
   update: z.object({
     name: z.string(),
+    description: z.string().optional(),
+    permissionNames: z.string().array().optional(),
   }),
 }
 
