74e65eae07e5e540ff178031ef5dbe998d30608f
mmulyana <mulyan.t20@gmail.com>
2024-07-04 02:57:46 +0700

    feat(roles-permission): implement repository

--- src/modules/roles-permission/controller.ts
+++ src/modules/roles-permission/controller.ts
@@ -1,80 +1,72 @@
 import { NextFunction, Request, Response } from 'express'
 import ApiResponse from '../../helper/api-response'
 import prisma from '../../../lib/prisma'
+import RolePermissionRepository from './repository'
 
 export default class RolesPermissionController {
   private response: ApiResponse = new ApiResponse()
+  private repository: RolePermissionRepository = new RolePermissionRepository()
 
-  create = async (req: Request, res: Response, next: NextFunction) => {
+  createHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { rolesId, permissionId, enabled } = req.body
-      await prisma.rolesPermission.create({
-        data: {
-          rolesId,
-          permissionId,
-          enabled,
-        },
-      })
+      const payload = {
+        rolesId,
+        permissionId,
+        enabled: enabled || false,
+      }
+      await this.repository.create(payload)
+
       return this.response.success(res, 'success create role permission')
     } catch (error) {
       next(error)
     }
   }
 
-  update = async (req: Request, res: Response, next: NextFunction) => {
+  updateHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
       const { rolesId, permissionId, enabled } = req.body
-      await prisma.rolesPermission.update({
-        data: {
-          enabled,
-          permissionId,
-          rolesId,
-        },
-        where: {
-          id: Number(id),
-        },
-      })
+      const payload = {
+        rolesId,
+        permissionId,
+        enabled: enabled || false,
+      }
+      await this.repository.update(payload, Number(id))
       this.response.success(res, 'success update role permission')
     } catch (error) {
       next(error)
     }
   }
 
-  delete = async (req: Request, res: Response, next: NextFunction) => {
+  deleteHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-
-      await prisma.rolesPermission.delete({
-        where: {
-          id: Number(id),
-        },
-      })
-
+      await this.repository.delete(Number(id))
       return this.response.success(res, 'success delete role permission')
     } catch (error) {
       next(error)
     }
   }
 
-  read = async (req: Request, res: Response, next: NextFunction) => {
+  readHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      const data = await prisma.rolesPermission.findUnique({
-        where: {
-          id: Number(id),
-        },
-      })
-
-      if (!data) {
-        throw new Error('role permission not found')
-      }
-
+      const data = this.repository.read(Number(id))
       return this.response.success(res, 'success get role permission', data)
     } catch (error: any) {
       if (error.message === 'role permission not found') {
         error.code = 404
       }
+      next(error)
+    }
+  }
+
+  readAllHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await this.repository.readAll()
+      this.response.success(res, 'success get role permission', data)
+    } catch (error) {
       next(error)
     }
   }
