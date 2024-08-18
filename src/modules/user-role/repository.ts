e501c3b96f99042381d1400bedf2f0a0abefde81
mmulyana <mulyan.t20@gmail.com>
2024-08-19 03:41:02 +0700

    feat(user-role): repository

---
+++ src/modules/user-role/repository.ts
@@ -0,0 +1,51 @@
+import db from '../../lib/db'
+
+export default class UserRoleRepository {
+  create = async (ids: number[], userId: number) => {
+    try {
+      await db.userRole.createMany({
+        data: ids.map((id) => ({
+          roleId: id,
+          userId: userId,
+        })),
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+  update = async (ids: number[], userId: number) => {
+    try {
+      const result = await db.$transaction(async (prisma) => {
+        await prisma.userRole.deleteMany({
+          where: {
+            userId: userId,
+          },
+        })
+
+        const updateRole = await prisma.userRole.createMany({
+          data: ids.map((id) => ({
+            roleId: id,
+            userId: userId,
+          })),
+        })
+
+        return updateRole
+      })
+
+      return result
+    } catch (error) {
+      throw error
+    }
+  }
+  delete = async (userId: number) => {
+    try {
+      await db.userRole.deleteMany({
+        where: {
+          userId: userId,
+        },
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+}
