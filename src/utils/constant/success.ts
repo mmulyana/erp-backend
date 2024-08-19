941ca8d12ea9cab2d1a08beff685a969c83b5226
mmulyana <mulyan.t20@gmail.com>
2024-08-20 04:16:15 +0700

    feat: new success message

--- src/utils/constant/success.ts
+++ src/utils/constant/success.ts
@@ -1,4 +1,11 @@
 export const MESSAGE_SUCCESS = {
   LOGIN: 'Berhasil Login',
   REGISTER: 'Berhasil Daftar, silahkan login',
+  POSITION: {
+    CREATE: 'Posisi baru berhasil ditambah',
+    UPDATE: 'Posisi ini berhasil dirubah',
+    DELETE: 'Posisi ini berhasil dihapus',
+    READ: 'Posisi berhasil didapatkan',
+    NOT_FOUND: 'Posisi tidak ada',
+  },
 }
