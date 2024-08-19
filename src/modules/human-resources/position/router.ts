0fa49db07d578e2b526e52da3c8ce8df7db5e744
mmulyana <mulyan.t20@gmail.com>
2024-08-20 04:17:05 +0700

    feat(position): router

---
+++ src/modules/human-resources/position/router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import PositionController from "./controller";
+import Validation from "../../../helper/validation";
+import positionSchema from "./schema";
+
+export default class PositionRoutes {
+    public router: Router
+    private controller: PositionController = new PositionController()
+    private createSchema: Validation = new Validation(positionSchema.create)
+    private updateSchema: Validation = new Validation(positionSchema.update)
+
+    constructor() {
+        this.router = Router()
+        this.register()
+    }
+
+    protected register() {
+        this.router.patch('/:id', this.updateSchema.validate, this.controller.updateHandler)
+        this.router.post('/:id', this.createSchema.validate, this.controller.createHandler)
+        this.router.delete('/:id', this.controller.deleteHandler)
+        this.router.get('/', this.controller.readAllHandler)
+        this.router.get('/:id', this.controller.readHandler)
+    }
+}
