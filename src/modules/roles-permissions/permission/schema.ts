5f40a39f7bf10651281e2eb21e3d6def863b38e2
mmulyana <mulyan.t20@gmail.com>
2024-08-19 19:07:23 +0700

    fix(permission): schema create and update

--- src/modules/roles-permissions/permission/schema.ts
+++ src/modules/roles-permissions/permission/schema.ts
@@ -3,9 +3,11 @@ import { z } from 'zod'
 const permissionSchema = {
   create: z.object({
     name: z.string(),
+    groupId: z.number()
   }),
   update: z.object({
     name: z.string(),
+    groupId: z.number()
   }),
 }
 
