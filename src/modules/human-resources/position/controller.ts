309641dae8bda7cd68d5f715bbecbf48b1a41282
mmulyana <mulyan.t20@gmail.com>
2024-08-20 04:45:50 +0700

    fix(position): controller

--- src/modules/human-resources/position/controller.ts
+++ src/modules/human-resources/position/controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import ApiResponse from '../../../helper/api-response'
 import PositionRepository from './repository'
 import { MESSAGE_SUCCESS } from '../../../utils/constant/success'
+import { MESSAGE_ERROR } from '../../../utils/constant/error'
 
 export default class PositionController {
   private response: ApiResponse = new ApiResponse()
@@ -10,7 +11,7 @@ export default class PositionController {
   createHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { name, description } = req.body
-      this.repository.create({ name, description })
+      await this.repository.create({ name, description })
       this.response.success(res, MESSAGE_SUCCESS.POSITION.CREATE)
     } catch (error) {
       next(error)
@@ -20,18 +21,24 @@ export default class PositionController {
     try {
       const { name, description } = req.body
       const { id } = req.params
-      this.repository.update(Number(id), { name, description })
+      await this.repository.update(Number(id), { name, description })
       this.response.success(res, MESSAGE_SUCCESS.POSITION.UPDATE)
-    } catch (error) {
+    } catch (error: any) {
+      if (error.message == MESSAGE_ERROR.POSITION.NOT_FOUND) {
+        error.code = 404
+      }
       next(error)
     }
   }
   deleteHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      this.repository.delete(Number(id))
+      await this.repository.delete(Number(id))
       this.response.success(res, MESSAGE_SUCCESS.POSITION.DELETE)
-    } catch (error) {
+    } catch (error: any) {
+      if (error.message == MESSAGE_ERROR.POSITION.NOT_FOUND) {
+        error.code = 404
+      }
       next(error)
     }
   }
@@ -40,7 +47,10 @@ export default class PositionController {
       const { id } = req.params
       const data = await this.repository.read(Number(id))
       this.response.success(res, MESSAGE_SUCCESS.POSITION.READ, data)
-    } catch (error) {
+    } catch (error: any) {
+      if (error.message == MESSAGE_ERROR.POSITION.NOT_FOUND) {
+        error.code = 404
+      }
       next(error)
     }
   }
