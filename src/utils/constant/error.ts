e7caecf74a2530fcc6f4f056517cc5507408bbc4
mmulyana <mulyan.t20@gmail.com>
2024-08-21 00:46:51 +0700

    feat: new message error

--- src/utils/constant/error.ts
+++ src/utils/constant/error.ts
@@ -11,6 +11,7 @@ export const MESSAGE_ERROR = {
   EMPLOYEE: {
     NOT_FOUND: "Pegawai tidak ditemukan",
     ADDRESS_NOT_FOUND: "Alamat pegawai ini tidak ditemukan",
-    CONTACT_NOT_FOUND: "Kontak pegawai ini tidak ditemukan"
+    CONTACT_NOT_FOUND: "Kontak pegawai ini tidak ditemukan",
+    POSITION_NOT_FOUND: "Posisi yang diambil tidak ada"
   }
 }
