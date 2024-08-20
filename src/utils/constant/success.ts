f795f01fb1780390765606ea9ee63b5039218b8e
mmulyana <mulyan.t20@gmail.com>
2024-08-20 22:20:13 +0700

    feat: new message success

--- src/utils/constant/success.ts
+++ src/utils/constant/success.ts
@@ -7,4 +7,25 @@ export const MESSAGE_SUCCESS = {
     DELETE: 'Posisi ini berhasil dihapus',
     READ: 'Posisi berhasil didapatkan',
   },
+  EMPLOYEE: {
+    CREATE: 'Pegawai berhasil ditambah',
+    UPDATE: 'Pegawai berhasil diubah',
+    DELETE: 'Pegawai berhasil dihapus',
+    READ: 'Pegawai berhasil didapatkan',
+    ADDRESS: {
+      CREATE: 'Alamat berhasil ditambahkan',
+      UPDATE: 'Alamat ini berhasil diubah',
+      REMOVE: 'Alamat ini berhasil dihapus',
+      READ: 'Alamat ini berhasil didapatkan',
+    },
+    CONTACT: {
+      CREATE: 'Kontak berhasil ditambahkan',
+      UPDATE: 'Kontak ini berhasil diubah',
+      REMOVE: 'Kontak ini berhasil dihapus',
+      READ: 'Kontak ini berhasil didapatkan',
+    },
+    POSITION: 'Berhasil menambahkan posisi',
+    ACTIVE: 'Berhasil mengaktifkan pegawai ini',
+    UNACTIVE: 'Berhasil menonaktifkan pegawai ini',
+  },
 }
