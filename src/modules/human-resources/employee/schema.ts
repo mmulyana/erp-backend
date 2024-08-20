aa7d9582153b90779ec75660725968c82e0ce9f2
mmulyana <mulyan.t20@gmail.com>
2024-08-20 22:20:00 +0700

    feat: new schema

--- src/modules/human-resources/employee/schema.ts
+++ src/modules/human-resources/employee/schema.ts
@@ -9,3 +9,41 @@ export const employeeSchema = z.object({
   status: z.string().optional(),
   positionId: z.number().optional(),
 })
+
+export const contactSchema = z.object({
+  type: z.string().optional(),
+  value: z.string(),
+})
+
+export const createAddressSchema = z.object({
+  type: z.string().optional(),
+  rt: z.string().optional(),
+  rw: z.string().optional(),
+  kampung: z.string().optional(),
+  desa: z.string().optional(),
+  kecamatan: z.string().optional(),
+  kebupaten: z.string().optional(),
+  provinsi: z.string().optional(),
+  kodePos: z.number().optional(),
+})
+
+export const updateAddressSchema = z.object({
+  id: z.number(),
+  type: z.string().optional(),
+  rt: z.string().optional(),
+  rw: z.string().optional(),
+  kampung: z.string().optional(),
+  desa: z.string().optional(),
+  kecamatan: z.string().optional(),
+  kebupaten: z.string().optional(),
+  provinsi: z.string().optional(),
+  kodePos: z.number().optional(),
+})
+
+export const deleteAddress = z.object({
+  id: z.number(),
+})
+
+export const positionSchema = z.object({
+  positionId: z.number(),
+})
