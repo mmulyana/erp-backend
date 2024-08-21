5980898912a6c99336a0c347d78c5387e49c589b
mmulyana <mulyan.t20@gmail.com>
2024-08-22 03:01:24 +0700

    feat(leave): controller

---
+++ src/modules/human-resources/leave/controller.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express'
+import { MESSAGE_SUCCESS } from '../../../utils/constant/success'
+import { MESSAGE_ERROR } from '../../../utils/constant/error'
+import ApiResponse from '../../../helper/api-response'
+import LeaveRepository from './repository'
+
+export default class EmployeeController {
+  private response: ApiResponse = new ApiResponse()
+  private repository: LeaveRepository = new LeaveRepository()
+
+  createHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await this.repository.create(req.body)
+      this.response.success(res, MESSAGE_SUCCESS.LEAVE.CREATE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  updateHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.update(Number(id), req.body)
+      this.response.success(res, MESSAGE_SUCCESS.LEAVE.UPDATE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  deleteHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.delete(Number(id))
+      this.response.success(res, MESSAGE_SUCCESS.LEAVE.DELETE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  readHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.query
+      const data = await this.repository.read(Number(id))
+      this.response.success(res, MESSAGE_SUCCESS.LEAVE.READ, data)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
