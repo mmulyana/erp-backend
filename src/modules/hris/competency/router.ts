c50c3d1aed058e41a64bdec5313fd3bae59b43f0
mmulyana <mulyan.t20@gmail.com>
2024-08-29 15:39:54 +0700

    feat(competency): router

---
+++ src/modules/hris/competency/router.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+import Validation from '../../../helper/validation'
+import { competencySchema } from './schema'
+import CompetencyController from './controller'
+
+export default class CompetencyRouter {
+  public router: Router
+  private competencySchema: Validation = new Validation(competencySchema)
+  private controller: CompetencyController = new CompetencyController()
+
+  constructor() {
+    this.router = Router()
+    this.register()
+  }
+
+  protected register() {
+    this.router.post('/', this.competencySchema.validate, this.controller.createHandler)
+    this.router.patch('/:id', this.competencySchema.validate, this.controller.updateHandler)
+    this.router.delete('/:id', this.controller.deleteHandler)
+    this.router.get('/', this.controller.readHandler)
+  }
+}
