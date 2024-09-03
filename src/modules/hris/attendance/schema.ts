c400740192c05f2b2c25b877515fa7d47bdfa14b
mmulyana <mulyan.t20@gmail.com>
2024-09-03 15:39:57 +0700

    feat(attendance): update schema

--- src/modules/hris/attendance/schema.ts
+++ src/modules/hris/attendance/schema.ts
@@ -3,9 +3,7 @@ import { z } from 'zod'
 export const attendanceSchema = z.object({
   employeeId: z.number(),
   date: z.string().date(),
-  timeIn: z.string(),
-  timeOut: z.string(),
-  period: z.number(),
+  total_hour: z.number(),
   isOnLeave: z.boolean().optional(),
   leaveId: z.number().optional(),
 })
