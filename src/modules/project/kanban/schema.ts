f03ad599775c1915d3107c634e857a2d2b00d5d0
mmulyana <mulyan.t20@gmail.com>
2024-09-15 00:44:41 +0700

    feat: schema kanban

---
+++ src/modules/project/kanban/schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const boardContainer = z.object({
+  name: z.string(),
+  color: z.string(),
+})
+export const boardItems = z.object({
+  containerId: z.string(),
+
+  // project
+  name: z.string(),
+  startDate: z.string().datetime(),
+  budget: z.number().optional(),
+  priority: z.enum(['urgent', 'high', 'medium', 'low']),
+  labels: z.number().array().optional(),
+  employees: z.number().array().optional(),
+  clientId: z.number().optional(),
+})
