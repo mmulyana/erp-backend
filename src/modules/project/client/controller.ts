ef59c192459bca515aa3c0370c48c655dd68626f
mmulyana <mulyan.t20@gmail.com>
2024-09-16 11:31:21 +0700

    feat(client): controller

---
+++ src/modules/project/client/controller.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from 'express'
+import ApiResponse from '../../../helper/api-response'
+import ClientRepository from './repository'
+import { MESSAGE_SUCCESS } from '../../../utils/constant/success'
+
+export default class ClientController {
+  private response: ApiResponse = new ApiResponse()
+  private repository: ClientRepository = new ClientRepository()
+
+  handleCreate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await this.repository.create(req.body)
+      return this.response.success(res, MESSAGE_SUCCESS.CLIENT.CREATE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleUpdate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.update(Number(id), req.body)
+      return this.response.success(res, MESSAGE_SUCCESS.CLIENT.UPDATE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleDelete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.delete(Number(id))
+      return this.response.success(res, MESSAGE_SUCCESS.CLIENT.DELETE)
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleRead = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await this.repository.read()
+      return this.response.success(res, MESSAGE_SUCCESS.CLIENT.READ, data)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
