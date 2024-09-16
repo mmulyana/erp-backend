d3e5a859f134947701891c6d101f5458b96c4e46
mmulyana <mulyan.t20@gmail.com>
2024-09-16 19:45:03 +0700

    fix(attendance): controller

--- src/modules/hris/attendance/controller.ts
+++ src/modules/hris/attendance/controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import ApiResponse from '../../../helper/api-response'
 import { MESSAGE_SUCCESS } from '../../../utils/constant/success'
 import AttendanceRepository from './repository'
+import { convertDateString } from '../../../utils/convert-date'
 
 export default class AttendanceController {
   private response: ApiResponse = new ApiResponse()
@@ -36,8 +37,11 @@ export default class AttendanceController {
   readHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id, date, name } = req.query
-      const startDate = date ? new Date(date as string).toUTCString() : new Date().toUTCString()
       const searchName = name ? String(name) : undefined
+      let startDate = new Date().toISOString()
+      if (date) {
+        startDate = convertDateString(date as string)
+      }
 
       const data = await this.repository.read(startDate, {
         search: searchName,
