5282f3dd0a973a16ff7e8a9a33ae889babfe9da1
mmulyana <mulyan.t20@gmail.com>
2024-09-26 04:51:18 +0700

    feat(brand): router

---
+++ src/modules/inventory/brand/router.ts
@@ -0,0 +1,33 @@
+import RouterWithFile from '../../../helper/router-with-file'
+import Validation from '../../../helper/validation'
+import { brandSchema } from './schema'
+import BrandController from './controller'
+import multer from 'multer'
+
+export default class BrandRouter extends RouterWithFile {
+  private schema: Validation = new Validation(brandSchema)
+  private controller: BrandController = new BrandController()
+
+  constructor(upload: multer.Multer) {
+    super(upload)
+    this.register()
+  }
+
+  protected register() {
+    this.router.post(
+      '/',
+      this.upload.single('photo'),
+      this.compressImage,
+      this.schema.validate,
+      this.controller.createHandler
+    )
+    this.router.patch(
+      '/:id',
+      this.upload.single('photo'),
+      this.compressImage,
+      this.controller.updateHandler
+    )
+    this.router.delete('/:id', this.controller.deleteHandler)
+    this.router.get('/', this.controller.readHandler)
+  }
+}
