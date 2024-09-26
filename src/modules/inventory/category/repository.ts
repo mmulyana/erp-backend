c2c79cd29bb78197760f5f182637b27c323dba66
mmulyana <mulyan.t20@gmail.com>
2024-09-26 19:31:21 +0700

    fix(category): handle data not found

--- src/modules/inventory/category/repository.ts
+++ src/modules/inventory/category/repository.ts
@@ -10,14 +10,16 @@ export default class CategoryRepository {
     }
   }
   update = async (id: number, payload: Category) => {
-    await db.goodsCategory.update({ data: payload, where: { id } })
     try {
+      await this.isExist(id)
+      await db.goodsCategory.update({ data: payload, where: { id } })
     } catch (error) {
       throw error
     }
   }
   delete = async (id: number) => {
     try {
+      await this.isExist(id)
       await db.goodsCategory.delete({ where: { id } })
     } catch (error) {
       throw error
@@ -44,5 +46,10 @@ export default class CategoryRepository {
     } catch (error) {
       throw error
     }
+  }
+
+  isExist = async (id: number) => {
+    const data = await db.goodsCategory.findUnique({ where: { id } })
+    if (!data) throw Error('kategori tidak ada')
   }
 }
