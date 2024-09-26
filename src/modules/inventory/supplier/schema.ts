65685dfb0c187c887271fc108c4f699ad56fcd84
mmulyana <mulyan.t20@gmail.com>
2024-09-26 09:35:22 +0700

    feat(supplier): update schema

--- src/modules/inventory/supplier/schema.ts
+++ src/modules/inventory/supplier/schema.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 export const supplierSchema = z.object({
   name: z.string(),
-  phone: z.string(),
+  phone: z.string().optional(),
   email: z.string().optional(),
-  address: z.string(),
+  address: z.string().optional(),
   status: z.enum(['active', 'nonactive']),
   tags: z.number().array().optional(),
 })
