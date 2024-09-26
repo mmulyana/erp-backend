349eb6ed1847ca2254417be5453e08c5b8fc486c
mmulyana <mulyan.t20@gmail.com>
2024-09-26 19:37:53 +0700

    fix(measurement): handle data not found

--- src/modules/inventory/measurement/repository.ts
+++ src/modules/inventory/measurement/repository.ts
@@ -10,14 +10,16 @@ export default class MeasurementsRepository {
     }
   }
   update = async (id: number, payload: Measurement) => {
-    await db.measurements.update({ data: payload, where: { id } })
     try {
+      await this.isExist(id)
+      await db.measurements.update({ data: payload, where: { id } })
     } catch (error) {
       throw error
     }
   }
   delete = async (id: number) => {
     try {
+      await this.isExist(id)
       await db.measurements.delete({ where: { id } })
     } catch (error) {
       throw error
@@ -44,5 +46,9 @@ export default class MeasurementsRepository {
     } catch (error) {
       throw error
     }
+  }
+  isExist = async (id: number) => {
+    const data = await db.measurements.findUnique({ where: { id } })
+    if (!data) throw Error('Ukuran tidak ditemukan')
   }
 }
