cf150b541e097fb05c7e20157315dab824ee8417
mmulyana <mulyan.t20@gmail.com>
2024-09-26 11:43:16 +0700

    feat(supplier): controller

---
+++ src/modules/inventory/supplier/controller.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express'
+import ApiResponse from '../../../helper/api-response'
+import Message from '../../../utils/constant/message'
+import SupplierRepository from './repository'
+
+export default class SupplierController {
+  private response: ApiResponse = new ApiResponse()
+  private repository: SupplierRepository = new SupplierRepository()
+  private message: Message = new Message('Label')
+
+  createHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await this.repository.create(req.body)
+      return this.response.success(res, this.message.successCreate())
+    } catch (error) {
+      throw error
+    }
+  }
+  updateHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.update(Number(id), req.body)
+      return this.response.success(res, this.message.successUpdate())
+    } catch (error) {
+      throw error
+    }
+  }
+  updateTags = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.updateTag(Number(id), req.body)
+      return this.response.success(res, this.message.successUpdate())
+    } catch (error) {
+      next(error)
+    }
+  }
+  deleteHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      await this.repository.delete(Number(id))
+      return this.response.success(res, this.message.successDelete())
+    } catch (error) {
+      throw error
+    }
+  }
+  readHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name } = req.query
+      const data = await this.repository.read(name?.toString())
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      throw error
+    }
+  }
+}
