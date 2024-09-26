93f15d5c5e43e713576f0d2ea5121c5681c7bd57
mmulyana <mulyan.t20@gmail.com>
2024-09-26 12:28:14 +0700

    feat(supplier): change to RouterWithFile

--- src/modules/inventory/supplier/router.ts
+++ src/modules/inventory/supplier/router.ts
@@ -2,22 +2,31 @@ import { Router } from 'express'
 import Validation from '../../../helper/validation'
 import Controller from './controller'
 import { supplierSchema, updateTagSchema } from './schema'
+import RouterWithFile from '../../../helper/router-with-file'
+import { Multer } from 'multer'
 
-export default class SupplierRouter {
-  public router: Router
+export default class SupplierRouter extends RouterWithFile {
   private schema: Validation = new Validation(supplierSchema)
   private updateTagSchema: Validation = new Validation(updateTagSchema)
   private controller: Controller = new Controller()
 
-  constructor() {
-    this.router = Router()
+  constructor(upload: Multer) {
+    super(upload)
     this.register()
   }
 
   protected register() {
-    this.router.post('/', this.schema.validate, this.controller.createHandler)
+    this.router.post(
+      '/',
+      this.upload.single('photo'),
+      this.compressImage,
+      this.schema.validate,
+      this.controller.createHandler
+    )
     this.router.patch(
       '/:id',
+      this.upload.single('photo'),
+      this.compressImage,
       this.schema.validate,
       this.controller.updateHandler
     )
