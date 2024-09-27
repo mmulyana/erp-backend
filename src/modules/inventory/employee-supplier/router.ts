86f59c6e3784bfc49cd9fb1a37dfa513fe517ad2
mmulyana <mulyan.t20@gmail.com>
2024-09-27 21:29:55 +0700

    feat(employee-supplier): update router

--- src/modules/inventory/employee-supplier/router.ts
+++ src/modules/inventory/employee-supplier/router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import Validation from '../../../helper/validation'
-import { schema } from './schema'
+import { schema, updateSchema } from './schema'
 import Controller from './controller'
 
 export default class EmployeeSupplierRouter {
   public router: Router
   private schema: Validation = new Validation(schema)
+  private updateSchema: Validation = new Validation(updateSchema)
   private controller: Controller = new Controller()
 
   constructor() {
@@ -17,7 +18,7 @@ export default class EmployeeSupplierRouter {
     this.router.post('/', this.schema.validate, this.controller.createHandler)
     this.router.patch(
       '/:id',
-      this.schema.validate,
+      this.updateSchema.validate,
       this.controller.updateHandler
     )
     this.router.delete('/:id', this.controller.deleteHandler)
