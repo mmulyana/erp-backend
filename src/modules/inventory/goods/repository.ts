ff1f009365a82ab5c70cd2e69bcc6ba1a5614c75
mmulyana <mulyan.t20@gmail.com>
2024-09-27 21:30:53 +0700

    fix(goods): update repository

--- src/modules/inventory/goods/repository.ts
+++ src/modules/inventory/goods/repository.ts
@@ -1,27 +1,74 @@
 import db from '../../../lib/db'
-import { Goods } from '@prisma/client'
 import { removeImg } from '../../../utils/file'
+import { Goods } from './schema'
 
 export default class GoodsRepository {
   create = async (payload: Goods & { photoUrl?: string }) => {
     try {
-      await db.goods.create({ data: payload })
+      await db.goods.create({
+        data: {
+          name: payload.name,
+          qty: Number(payload.qty),
+          available: Number(payload.qty),
+          minimum: Number(payload.minimum),
+          photoUrl: payload.photoUrl,
+          location: {
+            connect: {
+              id: Number(payload.locationId),
+            },
+          },
+          measurement: { connect: { id: Number(payload.measurementId) } },
+          brand: { connect: { id: Number(payload.brandId) } },
+          category: { connect: { id: Number(payload.categoryId) } },
+        },
+      })
     } catch (error) {
       throw error
     }
   }
   update = async (
     id: number,
-    payload: Partial<Goods> & { photoUrl?: string }
+    payload: Partial<Goods> & { newPhotoUrl?: string }
   ) => {
     try {
-      if (payload.photoUrl) {
+      if (payload.newPhotoUrl !== '') {
         const data = await db.brand.findUnique({ where: { id } })
-        if (data?.photoUrl) {
-          removeImg(data.photoUrl)
+        if (!!data?.photoUrl) {
+          await removeImg(data.photoUrl)
         }
       }
-      await db.goods.update({ data: payload, where: { id } })
+      await db.goods.update({
+        data: {
+          name: payload.name,
+          qty: Number(payload.qty),
+          available: Number(payload.qty),
+          minimum: Number(payload.minimum),
+          ...(payload.newPhotoUrl !== ''
+            ? { photoUrl: payload.newPhotoUrl }
+            : undefined),
+          location: {
+            connect: {
+              id: Number(payload.locationId),
+            },
+          },
+          ...(payload.measurementId !== ''
+            ? {
+                measurement: { connect: { id: Number(payload.measurementId) } },
+              }
+            : undefined),
+          ...(payload.brandId !== ''
+            ? {
+                brand: { connect: { id: Number(payload.brandId) } },
+              }
+            : undefined),
+          ...(payload.categoryId !== ''
+            ? {
+                category: { connect: { id: Number(payload.categoryId) } },
+              }
+            : undefined),
+        },
+        where: { id },
+      })
     } catch (error) {
       throw error
     }
