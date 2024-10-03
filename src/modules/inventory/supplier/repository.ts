682b3e0040c291c113079518fcbbffc2c2aa59d5
mmulyana <mulyan.t20@gmail.com>
2024-10-03 17:34:36 +0700

    feat: update repository

--- src/modules/inventory/supplier/repository.ts
+++ src/modules/inventory/supplier/repository.ts
@@ -4,54 +4,50 @@ import { Supplier } from './schema'
 
 export default class SupplierRepository {
   create = async (payload: Supplier & { photoUrl?: string }) => {
-    try {
-      await db.supplier.create({
-        data: {
-          address: payload.address,
-          name: payload.name,
-          phone: payload.phone,
-          status: payload.status,
-          ...(payload.photoUrl !== ''
-            ? { photoUrl: payload.photoUrl }
-            : undefined),
-          tags: {
-            create:
-              payload.tags?.map((tagId) => ({
-                tag: {
-                  connect: { id: Number(tagId) },
-                },
-              })) || [],
-          },
+    await db.supplier.create({
+      data: {
+        address: payload.address,
+        name: payload.name,
+        phone: payload.phone,
+        status: payload.status,
+        ...(payload.photoUrl !== ''
+          ? { photoUrl: payload.photoUrl }
+          : undefined),
+        tags: {
+          create:
+            payload.tags?.map((tagId) => ({
+              tag: {
+                connect: { id: Number(tagId) },
+              },
+            })) || [],
         },
-      })
-    } catch (error) {
-      throw error
-    }
+      },
+    })
   }
   update = async (id: number, payload: Supplier & { photoUrl?: string }) => {
-    try {
-      await this.isExist(id)
-      if (payload.photoUrl) {
-        const data = await db.supplier.findUnique({ where: { id } })
-        if (data?.photoUrl) {
-          removeImg(data?.photoUrl)
-        }
+    await this.isExist(id)
+    if (payload.photoUrl) {
+      const data = await db.supplier.findUnique({ where: { id } })
+      if (data?.photoUrl) {
+        removeImg(data?.photoUrl)
       }
+    }
 
-      await db.supplier.update({
-        data: {
-          address: payload.address,
-          name: payload.name,
-          phone: payload.phone,
-          status: payload.status,
-          ...(payload.photoUrl !== ''
-            ? { photoUrl: payload.photoUrl }
-            : undefined),
-        },
-        where: { id },
-      })
-    } catch (error) {
-      throw error
+    await db.supplier.update({
+      data: {
+        address: payload.address,
+        name: payload.name,
+        phone: payload.phone,
+        status: payload.status,
+        ...(payload.photoUrl !== ''
+          ? { photoUrl: payload.photoUrl }
+          : undefined),
+      },
+      where: { id },
+    })
+
+    if (!!payload.tags?.length) {
+      this.updateTag(id, { tagIds: payload.tags.map((item) => Number(item)) })
     }
   }
   updateTag = async (id: number, payload: { tagIds: number[] }) => {
@@ -103,46 +99,91 @@ export default class SupplierRepository {
     }
   }
   delete = async (id: number) => {
-    try {
-      await this.isExist(id)
-      const data = await db.supplier.findUnique({ where: { id } })
-      if (data?.photoUrl) {
-        removeImg(data?.photoUrl)
-      }
-      await db.supplier.delete({ where: { id } })
-    } catch (error) {
-      throw error
+    await this.isExist(id)
+    const data = await db.supplier.findUnique({
+      where: { id },
+      include: {
+        tags: true,
+        employees: true,
+        transaction: true,
+      },
+    })
+    if (data?.photoUrl) {
+      removeImg(data?.photoUrl)
     }
+
+    const transactionsIds = data?.transaction.map((item) => item.id)
+    const employeeIds = data?.employees.map((item) => item.id)
+    const tagsIds = data?.tags.map((item) => item.id)
+
+    // remove tags
+    await db.supplierToTag.deleteMany({
+      where: {
+        id: {
+          in: tagsIds,
+        },
+      },
+    })
+
+    // delete employees
+    await db.supplierEmployee.deleteMany({
+      where: {
+        id: {
+          in: employeeIds,
+        },
+      },
+    })
+
+    // change supplierId to null in transaction
+    await db.transactionGoods.updateMany({
+      data: {
+        supplierId: null,
+      },
+      where: {
+        id: {
+          in: transactionsIds,
+        },
+      },
+    })
+
+    await db.supplier.delete({ where: { id } })
   }
-  read = async (name?: string) => {
-    try {
-      const baseQuery = {
-        where: {},
-        include: {
-          employees: true,
-          tags: {
-            include: {
-              tag: true,
-            },
+  read = async (name?: string, tagId?: number) => {
+    const baseQuery = {
+      where: {},
+      include: {
+        employees: true,
+        tags: {
+          include: {
+            tag: true,
+          },
+        },
+      },
+    }
+
+    if (name) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [
+          { name: { contains: name.toLowerCase() } },
+          { name: { contains: name.toUpperCase() } },
+          { name: { contains: name } },
+        ],
+      }
+    }
+
+    if (tagId) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        tags: {
+          some: {
+            tagId: tagId,
           },
         },
       }
-
-      if (name) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [
-            { name: { contains: name.toLowerCase() } },
-            { name: { contains: name.toUpperCase() } },
-            { name: { contains: name } },
-          ],
-        }
-      }
-
-      return await db.supplier.findMany(baseQuery)
-    } catch (error) {
-      throw error
     }
+
+    return await db.supplier.findMany(baseQuery)
   }
   isExist = async (id: number) => {
     const data = await db.supplier.findUnique({ where: { id } })
