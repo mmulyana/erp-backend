8cc6599ad1d2e83447042690d02ae506d30514a5
mmulyana <mulyan.t20@gmail.com>
2024-10-03 10:49:05 +0700

    feat: update schema

--- src/modules/inventory/transaction/schema.ts
+++ src/modules/inventory/transaction/schema.ts
@@ -4,11 +4,11 @@ export const schema = z.object({
   goodsId: z.string(),
   qty: z.string(),
   price: z.string(),
-  supplierId: z.string(),
+  supplierId: z.string().optional(),
   date: z.string(),
   type: z.enum(['in', 'out', 'opname', 'borrowed', 'returned']),
   description: z.string().optional(),
   projectId: z.string().optional(),
-  isReturned: z.boolean().optional(),
+  isReturned: z.string().optional(),
 })
 export type Transaction = z.infer<typeof schema>
