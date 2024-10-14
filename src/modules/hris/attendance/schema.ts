70e2f08d70e01b73f0b620128724e70e8fec3d43
mmulyana <mulyan.t20@gmail.com>
2024-10-14 17:13:59 +0700

    feat: update schema

--- src/modules/hris/attendance/schema.ts
+++ src/modules/hris/attendance/schema.ts
@@ -2,14 +2,13 @@ import { z } from 'zod'
 
 export const createAttendanceSchema = z.object({
   employeeId: z.number(),
-  date: z.string().datetime(),
+  date: z.string(),
   total_hour: z.number(),
   type: z.string(),
-  leaveId: z.number().optional()
+  leaveId: z.number().optional(),
 })
-export const updateAttendanceSchema = z.object({
-  date: z.string().datetime(),
-  total_hour: z.number(),
-  type: z.string(),
-  leaveId: z.number().optional()
-})
+export const updateAttendanceSchema = createAttendanceSchema
+  .omit({
+    employeeId: true,
+  })
+  .partial()
