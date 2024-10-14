60cb0f7aa5c1fbbd8b823d93a2e5c10fdeb8d58d
mmulyana <mulyan.t20@gmail.com>
2024-10-14 17:14:39 +0700

    feat: update schema overtime

--- src/modules/hris/overtime/schema.ts
+++ src/modules/hris/overtime/schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const overtimeSchema = z.object({
   employeeId: z.number(),
-  date: z.string().datetime(),
+  date: z.string(),
   total_hour: z.number(),
   description: z.string().optional(),
 })
