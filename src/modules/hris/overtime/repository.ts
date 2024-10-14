8b26f2f8d4130573d908952ff67084150a45fd9e
mmulyana <mulyan.t20@gmail.com>
2024-10-14 17:15:29 +0700

    fix: repository overtime

--- src/modules/hris/overtime/repository.ts
+++ src/modules/hris/overtime/repository.ts
@@ -1,85 +1,63 @@
 import { z } from 'zod'
 import { overtimeSchema } from './schema'
 import db from '../../../lib/db'
+import { parse } from 'date-fns'
 
 type payload = z.infer<typeof overtimeSchema>
 export default class OvertimeRepository {
   create = async (payload: payload) => {
-    try {
-      await db.overtime.create({
-        data: { ...payload, date: new Date(payload.date).toISOString() },
-      })
-    } catch (error) {
-      throw error
-    }
+    await db.overtime.create({
+      data: {
+        ...payload,
+        date: parse(payload.date, 'dd-MM-yyyy', new Date()),
+      },
+    })
   }
   update = async (id: number, payload: payload) => {
-    try {
-      await db.overtime.update({
-        data: { ...payload, date: new Date(payload.date).toISOString() },
-        where: { id },
-      })
-    } catch (error) {
-      throw error
-    }
+    await db.overtime.update({
+      data: {
+        ...payload,
+        date: parse(payload.date, 'dd-MM-yyyy', new Date()),
+      },
+      where: { id },
+    })
   }
   delete = async (id: number) => {
-    try {
-      await db.overtime.delete({ where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await db.overtime.delete({ where: { id } })
   }
-  read = async (
-    startDate: Date,
-    { search, id }: { search?: string; id?: number }
-  ) => {
-    try {
-      if (!!id) {
-        const data = await db.overtime.findUnique({ where: { id } })
-        return data
-      }
-      const parsedDate = new Date(startDate)
-      if (isNaN(parsedDate.getTime())) {
-        throw new Error(
-          `Invalid startDate: ${startDate}. Please provide a valid date string.`
-        )
-      }
+  read = async (startDate: string, { search }: { search?: string }) => {
+    const parsedDate = new Date(startDate)
+    const dayStart = new Date(parsedDate.setHours(0, 0, 0, 0))
+    const dayEnd = new Date(parsedDate.setHours(23, 59, 59, 999))
 
-      const dayStart = new Date(parsedDate.setHours(0, 0, 0, 0))
-      const dayEnd = new Date(parsedDate.setHours(23, 59, 59, 999))
-
-      const baseQuery = {
-        where: {},
-        include: {
-          overtime: {
-            where: {
-              date: {
-                gte: dayStart,
-                lt: dayEnd,
-              },
+    const baseQuery = {
+      where: {},
+      include: {
+        overtime: {
+          where: {
+            date: {
+              gte: dayStart,
+              lt: dayEnd,
             },
           },
-          position: true,
         },
-      }
-
-      if (search) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [
-            { fullname: { contains: search.toLowerCase() } },
-            { fullname: { contains: search.toUpperCase() } },
-            { fullname: { contains: search } },
-          ],
-        }
-      }
-
-      const overtimes = await db.employee.findMany(baseQuery)
-      const data = overtimes.filter((employee) => !!employee.overtime.length)
-      return data
-    } catch (error) {
-      throw error
+        position: true,
+      },
     }
+
+    if (search) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [
+          { fullname: { contains: search.toLowerCase() } },
+          { fullname: { contains: search.toUpperCase() } },
+          { fullname: { contains: search } },
+        ],
+      }
+    }
+
+    const overtimes = await db.employee.findMany(baseQuery)
+    const data = overtimes.filter((employee) => !!employee.overtime.length)
+    return data
   }
 }
