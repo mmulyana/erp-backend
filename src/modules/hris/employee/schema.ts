6bcf8d70db5a44f00fd87a5b1b3eccc305953ce8
mmulyana <mulyan.t20@gmail.com>
2024-10-14 21:33:14 +0700

    feat: update

--- src/modules/hris/employee/schema.ts
+++ src/modules/hris/employee/schema.ts
@@ -1,12 +1,45 @@
 import { z } from 'zod'
 
+const EmployeeStatus = z.enum(['active', 'inactive'])
+const JoinedType = z.enum(['date', 'year'])
+const PayType = z.enum(['daily', 'monthly'])
+const EmploymentType = z.enum(['permanent', 'contract', 'partime'])
+const Gender = z.enum(['male', 'female'])
+const MaritalStatus = z.enum(['single', 'married', 'divorced'])
+
 export const employeeSchema = z.object({
   fullname: z.string(),
-  nickname: z.string().optional(),
-  hireDate: z.string().optional(),
-  salary: z.number().optional(),
-  status: z.enum(['active', 'nonactive']).optional(),
-  positionId: z.number().optional(),
+  status: EmployeeStatus.default('active'),
+  photo: z.string().optional().nullable(),
+  joined_at: z.string().optional().nullable(),
+  joined_type: JoinedType.default('date').optional().nullable(),
+  basic_salary: z.number().positive().optional().nullable(),
+  overtime_salary: z.number().positive().optional().nullable(),
+  pay_type: PayType.default('daily').optional().nullable(),
+  employment_type: EmploymentType.default('permanent').optional().nullable(),
+  place_of_birth: z.string().optional().nullable(),
+  birthdate: z.string().optional().nullable(),
+  gender: Gender.optional().nullable(),
+  marital_status: MaritalStatus.optional().nullable(),
+  religion: z.string().optional().nullable(),
+  positionId: z.number().int().positive().optional().nullable(),
+  addresses: z
+    .object({
+      type: z.enum(['domicile', 'origin', 'alternative']),
+      value: z.string(),
+    })
+    .array()
+    .optional()
+    .nullable(),
+  contacts: z
+    .object({
+      type: z.enum(['email', 'phoneNumber', 'socialMedia']),
+      value: z.string(),
+    })
+    .array()
+    .optional()
+    .nullable(),
+  competencyIds: z.number().array().optional().nullable(),
 })
 
 export const contactSchema = z.object({
@@ -17,14 +50,7 @@ export const contactSchema = z.object({
 
 export const addressSchema = z.object({
   type: z.enum(['domicile', 'origin']).optional(),
-  rt: z.string().optional(),
-  rw: z.string().optional(),
-  kampung: z.string().optional(),
-  desa: z.string().optional(),
-  kecamatan: z.string().optional(),
-  kebupaten: z.string().optional(),
-  provinsi: z.string().optional(),
-  kodePos: z.number().optional(),
+  value: z.string(),
 })
 
 export const deleteAddressSchema = z.object({
