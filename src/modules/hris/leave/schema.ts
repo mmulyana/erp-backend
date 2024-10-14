9653291a92dac8fa249f40dc23dcace70364c2a5
mmulyana <mulyan.t20@gmail.com>
2024-10-14 21:31:29 +0700

    feat: update

--- src/modules/hris/leave/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const leaveSchema = z.object({
-  employeeId: z.number(),
-  startDate: z.string().date(),
-  endDate: z.string().date(),
-  leaveType: z.string(),
-  description: z.string().optional(),
-})
