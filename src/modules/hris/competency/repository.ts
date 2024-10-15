5162f67c07cd3af9487bd5fa62bdf88e52220aa7
mmulyana <mulyan.t20@gmail.com>
2024-10-15 16:28:42 +0700

    chore: update

--- src/modules/hris/competency/repository.ts
+++ src/modules/hris/competency/repository.ts
@@ -5,44 +5,28 @@ import db from '../../../lib/db'
 type competency = z.infer<typeof competencySchema>
 export default class CompetencyRepository {
   create = async (data: competency) => {
-    try {
-      await db.competency.create({ data })
-    } catch (error) {
-      throw error
-    }
+    await db.competency.create({ data })
   }
   update = async (id: number, data: competency) => {
-    try {
-      await db.competency.update({
-        data,
-        where: { id },
-      })
-    } catch (error) {
-      throw error
-    }
+    await db.competency.update({
+      data,
+      where: { id },
+    })
   }
   delete = async (id: number) => {
-    try {
-      await db.competency.delete({ where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await db.competency.delete({ where: { id } })
   }
   read = async (name?: string) => {
-    try {
-      if (name !== '') {
-        const data = await db.competency.findMany({
-          where: {
-            name: {
-              contains: name,
-            },
+    if (name !== '') {
+      const data = await db.competency.findMany({
+        where: {
+          name: {
+            contains: name,
           },
-        })
-        return data
-      }
-      return await db.competency.findMany()
-    } catch (error) {
-      throw error
+        },
+      })
+      return data
     }
+    return await db.competency.findMany()
   }
 }
