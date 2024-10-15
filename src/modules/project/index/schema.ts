eda32064838352623ea6c7a5ad1960bf6abc579d
mmulyana <mulyan.t20@gmail.com>
2024-10-16 05:35:18 +0700

    feat: update

--- src/modules/project/index/schema.ts
+++ src/modules/project/index/schema.ts
@@ -2,22 +2,24 @@ import { z } from 'zod'
 
 export const createProjectSchema = z.object({
   name: z.string(),
-  startDate: z.string().datetime().optional(),
-  budget: z.number().optional(),
-  priority: z.enum(['urgent', 'high', 'medium', 'low']).optional(),
-  labels: z.number().array().optional(),
-  employees: z.number().array().optional(),
   containerId: z.string(),
-  clientId: z.number().optional(),
-})
-export const updateProjectSchema = z.object({
-  name: z.string(),
-  startDate: z.string().datetime().optional(),
-  budget: z.number().optional(),
-  priority: z.enum(['urgent', 'high', 'medium', 'low']).optional(),
+  date_created: z.string().datetime().optional(),
+  date_started: z.string().datetime().optional(),
+  date_ended: z.string().datetime().optional(),
+  net_value: z.number().optional(),
+  payment_status: z.number().optional(),
+  progress: z.number().optional(),
   labels: z.number().array().optional(),
   employees: z.number().array().optional(),
   clientId: z.number().optional(),
+  leadId: z.number().optional(),
 })
+export const updateProjectSchema = createProjectSchema
+  .omit({
+    name: true,
+  })
+  .extend({
+    name: z.string().optional(),
+  })
 
 export type Project = z.infer<typeof createProjectSchema>
