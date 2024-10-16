013c6cf3d93be6b2156787249a71f2711fccad8e
mmulyana <mulyan.t20@gmail.com>
2024-10-16 15:50:06 +0700

    fix: repository

--- src/modules/project/client/repository.ts
+++ src/modules/project/client/repository.ts
@@ -2,11 +2,16 @@ import db from '../../../lib/db'
 import { Client } from './schema'
 
 export default class ClientRepository {
-  create = async (data: Client) => {
-    await db.client.create({ data })
+  create = async (payload: Client) => {
+    await db.client.create({
+      data: { ...payload, companyId: Number(payload.companyId) || null },
+    })
   }
-  update = async (id: number, data: Client) => {
-    await db.client.update({ data, where: { id } })
+  update = async (id: number, payload: Client) => {
+    await db.client.update({
+      data: { ...payload, companyId: Number(payload.companyId) || null },
+      where: { id },
+    })
   }
   delete = async (id: number) => {
     await db.client.delete({ where: { id } })
