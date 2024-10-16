cbe47b6f44a919b3542df1a6f0669b2b07a09b5a
mmulyana <mulyan.t20@gmail.com>
2024-10-16 15:50:54 +0700

    feat: update

--- src/modules/project/company/repository.ts
+++ src/modules/project/company/repository.ts
@@ -1,33 +1,28 @@
 import db from '../../../lib/db'
+import { removeImg } from '../../../utils/file'
 import { Company } from './schema'
 
 export default class CompanyRepository {
-  create = async (data: Company) => {
-    try {
-      await db.companyClient.create({ data })
-    } catch (error) {
-      throw error
-    }
+  create = async (data: Company & { logo?: string }) => {
+    await db.companyClient.create({ data })
   }
-  update = async (id: number, data: Company) => {
-    try {
-      await db.companyClient.update({ data, where: { id } })
-    } catch (error) {
-      throw error
+  update = async (id: number, data: Partial<Company & { logo?: string }>) => {
+    if (data.logo) {
+      const data = await db.companyClient.findUnique({ where: { id } })
+      if (data?.logo) {
+        removeImg(data.logo)
+      }
     }
+    await db.companyClient.update({ data, where: { id } })
   }
   delete = async (id: number) => {
-    try {
-      await db.companyClient.delete({ where: { id } })
-    } catch (error) {
-      throw error
+    const data = await db.companyClient.findUnique({ where: { id } })
+    if (data?.logo) {
+      removeImg(data.logo)
     }
+    await db.companyClient.delete({ where: { id } })
   }
   read = async () => {
-    try {
-      return await db.companyClient.findMany()
-    } catch (error) {
-      throw error
-    }
+    return await db.companyClient.findMany()
   }
 }
