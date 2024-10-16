81389085bef9d1cafbf0959993b8282218101999
mmulyana <mulyan.t20@gmail.com>
2024-10-16 15:51:20 +0700

    refactor: company router

--- src/modules/project/company/router.ts
+++ src/modules/project/company/router.ts
@@ -2,20 +2,21 @@ import { Router } from 'express'
 import Validation from '../../../helper/validation'
 import ClientController from './controller'
 import { companySchema } from './schema'
+import RouterWithFile from '../../../helper/router-with-file'
+import { MulterConfig } from '../../../utils/multer-config'
 
-export default class CompanyRouter {
-  public router: Router
+export default class CompanyRouter extends RouterWithFile {
   private controller: ClientController = new ClientController()
   private companySchema: Validation = new Validation(companySchema)
 
-  constructor() {
-    this.router = Router()
+  constructor(multer: MulterConfig) {
+    super(multer.uploadImg, 'company')
     this.register()
   }
 
   protected register() {
-    this.router.post('/', this.companySchema.validate, this.controller.handleCreate)
-    this.router.patch('/:id', this.companySchema.validate, this.controller.handleUpdate)
+    this.router.post('/', this.upload.single('photo'), this.compressImage, this.companySchema.validate, this.controller.handleCreate)
+    this.router.patch('/:id', this.upload.single('photo'), this.compressImage, this.companySchema.validate, this.controller.handleUpdate)
     this.router.delete('/:id', this.controller.handleDelete)
     this.router.get('/', this.controller.handleRead)
   }
