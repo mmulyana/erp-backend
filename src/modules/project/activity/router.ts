2f16d0b70f4291b2a0d5d29bc24aa4b91a74f36f
mmulyana <mulyan.t20@gmail.com>
2024-10-16 15:48:49 +0700

    feat: update

--- src/modules/project/activity/router.ts
+++ src/modules/project/activity/router.ts
@@ -1,21 +1,21 @@
-import { Router } from 'express'
+import RouterWithFile from '../../../helper/router-with-file'
+import { MulterConfig } from '../../../utils/multer-config'
 import Validation from '../../../helper/validation'
 import ClientController from './controller'
-import { commentSchema } from './schema'
+import { activitySchema } from './schema'
 
-export default class ClientRouter {
-  public router: Router
+export default class ActivityRouter extends RouterWithFile {
   private controller: ClientController = new ClientController()
-  private commentSchema: Validation = new Validation(commentSchema)
+  private commentSchema: Validation = new Validation(activitySchema)
 
-  constructor() {
-    this.router = Router()
+  constructor(multer: MulterConfig) {
+    super(multer.uploadImg, 'activity')
     this.register()
   }
 
   protected register() {
-    this.router.post('/', this.commentSchema.validate, this.controller.handleCreate)
-    this.router.patch('/:id', this.commentSchema.validate, this.controller.handleUpdate)
+    this.router.post('/', this.upload.array('photos'), this.commentSchema.validate, this.controller.handleCreate)
+    this.router.patch('/:id', this.upload.array('photos'), this.commentSchema.validate, this.controller.handleUpdate)
     this.router.delete('/:id', this.controller.handleDelete)
     this.router.get('/', this.controller.handleRead)
   }
