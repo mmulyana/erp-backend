50436c2704c4e22fdc74905fe2d2a0429ed8eaa3
mmulyana <mulyan.t20@gmail.com>
2024-10-21 21:42:49 +0700

    refactor: update competency controller

--- src/modules/hris/competency/controller.ts
+++ src/modules/hris/competency/controller.ts
@@ -2,15 +2,19 @@ import { NextFunction, Request, Response } from 'express'
 import ApiResponse from '../../../helper/api-response'
 import CompetencyRepository from './repository'
 import { MESSAGE_SUCCESS } from '../../../utils/constant/success'
+import BaseController from '../../../helper/base-controller'
 
-export default class CompetencyController {
-  private response: ApiResponse = new ApiResponse()
+export default class CompetencyController extends BaseController {
   private repository: CompetencyRepository = new CompetencyRepository()
+
+  constructor() {
+    super('Kompetensi')
+  }
 
   createHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       await this.repository.create(req.body)
-      return this.response.success(res, MESSAGE_SUCCESS.COMPETENCY.CREATE)
+      return this.response.success(res, this.message.successCreate())
     } catch (error) {
       next(error)
     }
@@ -18,8 +22,8 @@ export default class CompetencyController {
   updateHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      await this.repository.update(Number(id), req.body)
-      return this.response.success(res, MESSAGE_SUCCESS.COMPETENCY.UPDATE)
+      const data = await this.repository.update(Number(id), req.body)
+      return this.response.success(res, this.message.successUpdate(), data)
     } catch (error) {
       next(error)
     }
@@ -28,7 +32,7 @@ export default class CompetencyController {
     try {
       const { id } = req.params
       await this.repository.delete(Number(id))
-      return this.response.success(res, MESSAGE_SUCCESS.COMPETENCY.DELETE)
+      return this.response.success(res, this.message.successDelete())
     } catch (error) {
       next(error)
     }
@@ -37,7 +41,16 @@ export default class CompetencyController {
     try {
       const { name } = req.query
       const data = await this.repository.read(name?.toString())
-      return this.response.success(res, MESSAGE_SUCCESS.COMPETENCY.READ, data)
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  readOneHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.readOne(Number(id))
+      return this.response.success(res, this.message.successRead(), data)
     } catch (error) {
       next(error)
     }
