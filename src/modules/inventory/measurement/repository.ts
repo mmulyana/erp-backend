153c25603ce4ee9e63b6831a47653d65783d625f
mmulyana <mulyan.t20@gmail.com>
2024-10-21 23:48:51 +0700

    feat: update repository

--- src/modules/inventory/measurement/repository.ts
+++ src/modules/inventory/measurement/repository.ts
@@ -3,49 +3,62 @@ import { Measurement } from './schema'
 
 export default class MeasurementsRepository {
   create = async (payload: Measurement) => {
-    try {
-      await db.measurements.create({ data: payload })
-    } catch (error) {
-      throw error
-    }
+    await db.measurements.create({ data: payload })
   }
   update = async (id: number, payload: Measurement) => {
-    try {
-      await this.isExist(id)
-      await db.measurements.update({ data: payload, where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await this.isExist(id)
+    return await db.measurements.update({
+      data: payload,
+      where: { id },
+      include: {
+        _count: {
+          select: {
+            goods: true,
+          },
+        },
+      },
+    })
   }
   delete = async (id: number) => {
-    try {
-      await this.isExist(id)
-      await db.measurements.delete({ where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await this.isExist(id)
+    await db.measurements.delete({ where: { id } })
   }
   read = async (name?: string) => {
-    try {
-      const baseQuery = {
-        where: {},
-      }
-
-      if (name) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [
-            { name: { contains: name.toLowerCase() } },
-            { name: { contains: name.toUpperCase() } },
-            { name: { contains: name } },
-          ],
-        }
-      }
-
-      return await db.measurements.findMany(baseQuery)
-    } catch (error) {
-      throw error
+    const baseQuery = {
+      where: {},
+      include: {
+        _count: {
+          select: {
+            goods: true,
+          },
+        },
+      },
     }
+
+    if (name) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [
+          { name: { contains: name.toLowerCase() } },
+          { name: { contains: name.toUpperCase() } },
+          { name: { contains: name } },
+        ],
+      }
+    }
+
+    return await db.measurements.findMany(baseQuery)
+  }
+  readOne = async (id: number) => {
+    return await db.measurements.findUnique({
+      where: { id },
+      include: {
+        _count: {
+          select: {
+            goods: true,
+          },
+        },
+      },
+    })
   }
   isExist = async (id: number) => {
     const data = await db.measurements.findUnique({ where: { id } })
