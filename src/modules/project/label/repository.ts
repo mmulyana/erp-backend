015a637ca275228c1bc83c39c3d2725b067032d8
mmulyana <mulyan.t20@gmail.com>
2024-10-21 21:43:37 +0700

    feat: update repository

--- src/modules/project/label/repository.ts
+++ src/modules/project/label/repository.ts
@@ -3,31 +3,53 @@ import { Label } from './schema'
 
 export default class LabelRepository {
   create = async (data: Label) => {
-    try {
-      await db.projectLabel.create({ data })
-    } catch (error) {
-      throw error
-    }
+    await db.projectLabel.create({ data })
   }
   update = async (id: number, data: Label) => {
-    try {
-      await db.projectLabel.update({ data, where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await db.projectLabel.update({
+      data,
+      where: { id },
+      include: {
+        _count: {
+          select: {
+            projects: true,
+          },
+        },
+      },
+    })
   }
   delete = async (id: number) => {
-    try {
-      await db.projectLabel.delete({ where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await db.projectLabel.delete({ where: { id } })
   }
-  read = async () => {
-    try {
-      return await db.projectLabel.findMany()
-    } catch (error) {
-      throw error
+  read = async (name?: string) => {
+    if (name !== '') {
+      const data = await db.projectLabel.findMany({
+        where: {
+          name: {
+            contains: name,
+          },
+        },
+        include: {
+          _count: {
+            select: {
+              projects: true,
+            },
+          },
+        },
+      })
+      return data
     }
+    return await db.projectLabel.findMany({
+      include: {
+        _count: {
+          select: {
+            projects: true,
+          },
+        },
+      },
+    })
+  }
+  readOne = async (id: number) => {
+    return await db.projectLabel.findUnique({ where: { id } })
   }
 }
