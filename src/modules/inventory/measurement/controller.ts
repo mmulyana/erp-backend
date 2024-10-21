2a51f531c857feeb63c4bc19e73ec21078285e1e
mmulyana <mulyan.t20@gmail.com>
2024-10-21 23:48:36 +0700

    refactor: controller measurement

--- src/modules/inventory/measurement/controller.ts
+++ src/modules/inventory/measurement/controller.ts
@@ -2,11 +2,14 @@ import { NextFunction, Request, Response } from 'express'
 import ApiResponse from '../../../helper/api-response'
 import Message from '../../../utils/constant/message'
 import MeasurementsRepository from './repository'
+import BaseController from '../../../helper/base-controller'
 
-export default class MeasurementController {
-  private response: ApiResponse = new ApiResponse()
+export default class MeasurementController extends BaseController {
   private repository: MeasurementsRepository = new MeasurementsRepository()
-  private message: Message = new Message('Ukuran')
+
+  constructor() {
+    super('Ukuran')
+  }
 
   createHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -38,6 +41,15 @@ export default class MeasurementController {
     try {
       const { name } = req.query
       const data = await this.repository.read(name?.toString())
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  readOneHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.readOne(Number(id))
       return this.response.success(res, this.message.successRead(), data)
     } catch (error) {
       next(error)
