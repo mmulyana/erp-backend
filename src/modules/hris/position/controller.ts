8d243ce18a315105718446a7b1f18c6ecb5d8cbc
mmulyana <mulyan.t20@gmail.com>
2024-10-22 22:11:19 +0700

    feat: update

--- src/modules/hris/position/controller.ts
+++ src/modules/hris/position/controller.ts
@@ -46,7 +46,8 @@ export default class PositionController extends BaseController {
   }
   readAllHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = await this.repository.readAll()
+      const { name } = req.query
+      const data = await this.repository.readAll(name?.toString())
       return this.response.success(res, this.message.successRead(), data)
     } catch (error) {
       next(error)
