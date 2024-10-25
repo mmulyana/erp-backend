562100e0bfea4ebfb4e67257f87c742112366d4f
mmulyana <mulyan.t20@gmail.com>
2024-10-25 19:25:16 +0700

    feat: update schema zod

--- src/modules/project/index/schema.ts
+++ src/modules/project/index/schema.ts
@@ -3,17 +3,18 @@ import { z } from 'zod'
 export const createProjectSchema = z.object({
   name: z.string(),
   containerId: z.string(),
-  date_created: z.string().datetime().optional(),
+  progress: z.number().optional().nullable().default(0),
+  payment_status: z.number().optional().nullable().default(0),
+  description: z.string().optional().nullable(),
+  // date_created: z.string().datetime().optional(),
   date_started: z.string().datetime().optional(),
   date_ended: z.string().datetime().optional(),
-  net_value: z.number().optional(),
-  payment_status: z.number().optional(),
-  progress: z.number().optional(),
+  isArchive: z.boolean().optional().default(false),
+  net_value: z.number().optional().nullable(),
+  leadId: z.number().optional().nullable(),
+  clientId: z.number().optional().nullable(),
   labels: z.number().array().optional(),
   employees: z.number().array().optional(),
-  clientId: z.number().optional(),
-  leadId: z.number().optional(),
-  description: z.string().optional(),
 })
 export const updateProjectSchema = createProjectSchema.partial()
 
