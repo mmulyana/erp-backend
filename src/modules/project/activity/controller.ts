87925f94de5da23d8757376f92bc36d5ec6c782c
mmulyana <mulyan.t20@gmail.com>
2024-10-28 08:20:53 +0700

    feat: update

--- src/modules/project/activity/controller.ts
+++ src/modules/project/activity/controller.ts
@@ -11,8 +11,8 @@ export default class ActivityController extends BaseController {
 
   handleCreate = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await this.repository.create(req.body)
-      return this.response.success(res, this.message.successCreate())
+      const data = await this.repository.create(req.body)
+      return this.response.success(res, this.message.successCreate(), data)
     } catch (error) {
       next(error)
     }
@@ -20,8 +20,8 @@ export default class ActivityController extends BaseController {
   handleUpdate = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      await this.repository.update(Number(id), req.body)
-      return this.response.success(res, this.message.successUpdate())
+      const data = await this.repository.update(Number(id), req.body)
+      return this.response.success(res, this.message.successUpdate(), data)
     } catch (error) {
       next(error)
     }
@@ -29,15 +29,16 @@ export default class ActivityController extends BaseController {
   handleDelete = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      await this.repository.delete(Number(id))
-      return this.response.success(res, this.message.successDelete())
+      const data = await this.repository.delete(Number(id))
+      return this.response.success(res, this.message.successDelete(), data)
     } catch (error) {
       next(error)
     }
   }
   handleRead = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = await this.repository.read()
+      const { id } = req.query
+      const data = await this.repository.read(Number(id))
       return this.response.success(res, this.message.successRead(), data)
     } catch (error) {
       next(error)
