647d304a77e068f0fbcedb9eac5658224eee04d6
mmulyana <mulyan.t20@gmail.com>
2024-10-29 22:43:36 +0700

    feat: update

--- src/modules/project/activity/router.ts
+++ src/modules/project/activity/router.ts
@@ -2,11 +2,16 @@ import RouterWithFile from '../../../helper/router-with-file'
 import { MulterConfig } from '../../../utils/multer-config'
 import Validation from '../../../helper/validation'
 import ClientController from './controller'
-import { activitySchema, removeAttachmentSchema } from './schema'
+import { 
+  activitySchema, 
+  removeAttachmentSchema, 
+  updateActivitySchema 
+} from './schema'
 
 export default class ActivityRouter extends RouterWithFile {
   private controller: ClientController = new ClientController()
   private commentSchema: Validation = new Validation(activitySchema)
+  private updateSchema: Validation = new Validation(updateActivitySchema)
   private removeAttachmentSchema: Validation = new Validation(removeAttachmentSchema)
 
   constructor(multer: MulterConfig) {
@@ -16,13 +21,13 @@ export default class ActivityRouter extends RouterWithFile {
 
   protected register() {
     this.router.post('/', this.commentSchema.validate, this.controller.handleCreate)
-    this.router.patch('/:id', this.commentSchema.validate, this.controller.handleUpdate)
+    this.router.patch('/:id', this.updateSchema.validate, this.controller.handleUpdate)
     this.router.delete('/:id', this.controller.handleDelete)
     this.router.get('/', this.controller.handleRead)
 
     this.router.post('/toggle/like', this.controller.handleToggleLike)
     this.router.post('/:id/upload/photo', this.upload.array('photos', 5), this.handleMultipleImage, this.controller.handleUploadAttachments)
     this.router.patch('/photo/:attachmentId', this.upload.single('photo'), this.controller.handleChangeAttachment)
-    this.router.delete('/photo', this.removeAttachmentSchema.validate, this.controller.handleRemoveAttachments)
+    this.router.delete('/photo/remove', this.removeAttachmentSchema.validate, this.controller.handleRemoveAttachments)
   }
 }
