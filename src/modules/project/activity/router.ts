0997ee24d375cdedfdbb66956d763a2c0d0a3c77
mmulyana <mulyan.t20@gmail.com>
2024-10-29 15:21:43 +0700

    feat: update

--- src/modules/project/activity/router.ts
+++ src/modules/project/activity/router.ts
@@ -2,11 +2,12 @@ import RouterWithFile from '../../../helper/router-with-file'
 import { MulterConfig } from '../../../utils/multer-config'
 import Validation from '../../../helper/validation'
 import ClientController from './controller'
-import { activitySchema } from './schema'
+import { activitySchema, removeAttachmentSchema } from './schema'
 
 export default class ActivityRouter extends RouterWithFile {
   private controller: ClientController = new ClientController()
   private commentSchema: Validation = new Validation(activitySchema)
+  private removeAttachmentSchema: Validation = new Validation(removeAttachmentSchema)
 
   constructor(multer: MulterConfig) {
     super(multer.uploadImg, 'activity')
@@ -20,6 +21,8 @@ export default class ActivityRouter extends RouterWithFile {
     this.router.get('/', this.controller.handleRead)
 
     this.router.post('/toggle/like', this.controller.handleToggleLike)
-    this.router.post('/:id/upload/photo', this.upload.array('photos', 5), this.handleMultipleImage, this.controller.handleUploadAttachment)
+    this.router.post('/:id/upload/photo', this.upload.array('photos', 5), this.handleMultipleImage, this.controller.handleUploadAttachments)
+    this.router.patch('/photo/:attachmentId', this.upload.single('photo'), this.controller.handleChangeAttachment)
+    this.router.delete('/photo', this.removeAttachmentSchema.validate, this.controller.handleRemoveAttachments)
   }
 }
