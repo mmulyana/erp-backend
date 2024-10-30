741b0071a1dc9afde805f4749e1244062f8ed363
mmulyana <mulyan.t20@gmail.com>
2024-10-31 02:22:13 +0700

    fix: schema

--- src/modules/project/attachment/schema.ts
+++ src/modules/project/attachment/schema.ts
@@ -2,9 +2,10 @@ import { z } from 'zod'
 
 export const attachmentSchema = z.object({
   name: z.string(),
-  uploaded_by: z.number().optional().nullable(),
-  projectId: z.number(),
+  uploaded_by: z.string().optional().nullable(),
+  projectId: z.string(),
   isSecret: z.boolean().default(false),
+  type: z.string().optional(),
 })
 export const updateSchema = attachmentSchema.partial()
 export type Attachment = z.infer<typeof attachmentSchema>
