7db96dce76dd0bf5991261ab1f465f3c2c7bb51a
mmulyana <mulyan.t20@gmail.com>
2024-10-31 04:04:56 +0700

    fix: repository

--- src/modules/project/estimate/repository.ts
+++ src/modules/project/estimate/repository.ts
@@ -1,25 +1,68 @@
 import db from '../../../lib/db'
-import { Estimate } from './schema'
 
+interface EstimateItem {
+  id: number | null
+  name: string
+  price: number
+  qty: number
+}
+
+interface Payload {
+  items: EstimateItem[]
+}
 export default class EstimateRepository {
-  create = async (data: Estimate) => {
-    return await db.projectEstimate.create({
-      data,
+  saveEstimate = async (projectId: number, payload: Payload) => {
+    const itemsToCreate = payload.items.filter((item) => item.id === null)
+    const itemsToUpdate = payload.items.filter((item) => item.id !== null)
+
+    await db.$transaction(async (tx) => {
+      const createdItems = await Promise.all(
+        itemsToCreate.map((item) =>
+          tx.projectEstimate.create({
+            data: {
+              name: item.name,
+              price: item.price,
+              qty: item.qty,
+              projectId: projectId,
+            },
+          })
+        )
+      )
+
+      const updatedItems = await Promise.all(
+        itemsToUpdate.map((item) =>
+          tx.projectEstimate.update({
+            where: {
+              id: item.id as number,
+            },
+            data: {
+              name: item.name,
+              price: item.price,
+              qty: item.qty,
+              projectId: projectId,
+            },
+          })
+        )
+      )
+
+      return [...createdItems, ...updatedItems]
     })
+
+    return { projectId }
   }
-  update = async (id: number, data: Partial<Estimate>) => {
-    return await db.projectEstimate.update({
-      data,
-      where: { id },
+  deleteMany = async ({ ids }: { ids: number[] }) => {
+    return await db.projectEstimate.deleteMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
     })
-  }
-  delete = async (id: number) => {
-    return await db.projectEstimate.delete({ where: { id } })
   }
   read = async (id: number) => {
     return await db.projectEstimate.findMany({
       where: {
-        id,
+        projectId: id,
       },
     })
   }
