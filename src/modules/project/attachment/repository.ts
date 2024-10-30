491d363a600dac973e4a123874427e20c4c08451
mmulyana <mulyan.t20@gmail.com>
2024-10-30 15:29:09 +0700

    feat: update

---
+++ src/modules/project/attachment/repository.ts
@@ -0,0 +1,64 @@
+import { deleteFile, PATHS } from '../../../utils/file'
+import { Attachment } from './schema'
+import db from '../../../lib/db'
+
+export default class AttachmentRepository {
+  createAttachment = async (
+    payload: Attachment & { file: Express.Multer.File | null }
+  ) => {
+    return await db.projectAttachment.create({
+      data: {
+        name: payload.name,
+        type: payload.file?.mimetype,
+        uploaded_by: payload.uploaded_by,
+        projectId: payload.projectId,
+        isSecret: payload.isSecret,
+        file: payload.file?.filename as string,
+        uploaded_at: new Date(),
+      },
+      include: {
+        user: true,
+        project: true,
+      },
+    })
+  }
+
+  readAttachment = async (id: number) => {
+    await db.projectAttachment.findUnique({
+      where: { id },
+      include: {
+        project: true,
+        user: true,
+      },
+    })
+  }
+
+  updateAttachment = async (
+    id: number,
+    payload: Partial<Attachment> & { newFile: string | null }
+  ) => {
+    const existing = await db.projectAttachment.findUnique({ where: { id } })
+    if (payload.newFile !== '' && existing?.file) {
+      deleteFile(existing.file, PATHS.FILES)
+    }
+
+    return await db.projectAttachment.update({
+      where: { id },
+      data: payload,
+    })
+  }
+
+  deleteAttachment = async (id: number) => {
+    const existing = await db.projectAttachment.findUnique({ where: { id } })
+    if (existing?.file) {
+      deleteFile(existing.file, PATHS.FILES)
+    }
+
+    return await db.projectAttachment.delete({
+      where: { id },
+      select: {
+        projectId: true,
+      },
+    })
+  }
+}
