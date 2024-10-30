51c9e2471e0237c1fd1d40ec1f9365bf3396b785
mmulyana <mulyan.t20@gmail.com>
2024-10-30 15:29:18 +0700

    feat: update

---
+++ src/modules/project/attachment/controller.ts
@@ -0,0 +1,86 @@
+import BaseController from '../../../helper/base-controller'
+import { NextFunction, Request, Response } from 'express'
+import Repository from './repository'
+
+export default class AttachmentController extends BaseController {
+  private repository: Repository = new Repository()
+
+  constructor() {
+    super('Lampiran')
+  }
+
+  handleCreate = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const data = await this.repository.createAttachment({
+        ...req.body,
+        file: req.file,
+        projectId: Number(req.body.projectId),
+      })
+      return this.response.success(
+        res,
+        this.message.successCreate(),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleUpdate = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.updateAttachment(Number(id), {
+        ...req.body,
+        newFile: req.file ?? null,
+      })
+      return this.response.success(
+        res,
+        this.message.successUpdate(),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleRead = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.readAttachment(Number(id))
+      return this.response.success(
+        res,
+        this.message.successRead(),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleDelete = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.deleteAttachment(Number(id))
+      return this.response.success(
+        res,
+        this.message.successDelete(),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+}
