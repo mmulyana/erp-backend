11c8dd6e20f12f6f4341d7d302cebe9e9204a85f
mmulyana <mulyan.t20@gmail.com>
2024-11-01 03:49:16 +0700

    feat: update

--- src/modules/project/company/controller.ts
+++ src/modules/project/company/controller.ts
@@ -20,11 +20,11 @@ export default class CompanyController extends BaseController {
   handleUpdate = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params
-      await this.repository.update(Number(id), {
+      const data = await this.repository.update(Number(id), {
         ...req.body,
         logo: req.file?.filename,
       })
-      return this.response.success(res, this.message.successUpdate())
+      return this.response.success(res, this.message.successUpdate(), data)
     } catch (error) {
       next(error)
     }
@@ -41,6 +41,15 @@ export default class CompanyController extends BaseController {
   handleRead = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data = await this.repository.read()
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  handleReadOne = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const data = await this.repository.readOne(Number(id))
       return this.response.success(res, this.message.successRead(), data)
     } catch (error) {
       next(error)
