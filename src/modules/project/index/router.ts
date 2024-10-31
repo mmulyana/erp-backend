9d057f22c947528a5857db6575076ff3d6a54a8e
mmulyana <mulyan.t20@gmail.com>
2024-10-31 23:47:36 +0700

    feat: update router

--- src/modules/project/index/router.ts
+++ src/modules/project/index/router.ts
@@ -1,12 +1,19 @@
-import { Router } from 'express'
 import Validation from '../../../helper/validation'
-import { createProjectSchema, updateProjectSchema } from './schema'
 import ProjectController from './controller'
+import { Router } from 'express'
+import {
+  addEmployeeSchema,
+  addLabelSchema,
+  createProjectSchema,
+  updateProjectSchema,
+} from './schema'
 
 export default class ProjectRouter {
   public router: Router
   private createProjectSchema: Validation = new Validation(createProjectSchema)
   private updateProjectSchema: Validation = new Validation(updateProjectSchema)
+  private addLabel: Validation = new Validation(addLabelSchema)
+  private addEmployee: Validation = new Validation(addEmployeeSchema)
   private controller: ProjectController = new ProjectController()
 
   constructor() {
@@ -15,9 +22,22 @@ export default class ProjectRouter {
   }
 
   protected register() {
-    this.router.post('/',this.createProjectSchema.validate,this.controller.handleCreate)
-    this.router.patch('/:id',this.updateProjectSchema.validate,this.controller.handleUpdate)
+    this.router.post(
+      '/',
+      this.createProjectSchema.validate,
+      this.controller.handleCreate
+    )
+    this.router.patch(
+      '/:id',
+      this.updateProjectSchema.validate,
+      this.controller.handleUpdate
+    )
     this.router.delete('/:id', this.controller.handleDelete)
     this.router.get('/', this.controller.handleRead)
+
+    this.router.patch('/employee/add/:projectId', this.addEmployee.validate, this.controller.handleAddEmployee)
+    this.router.patch('/employee/remove/:id', this.controller.handleRemoveEmployee)
+    this.router.patch('/label/add/:projectId', this.addLabel.validate, this.controller.handleAddLabel)
+    this.router.patch('/label/remove/:id', this.controller.handleRemoveLabel)
   }
 }
