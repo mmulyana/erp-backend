3c9ffab14062b599e6f3483ca2bab444da3d3fd0
mmulyana <mulyan.t20@gmail.com>
2024-11-03 02:24:42 +0700

    fix: repository

--- src/modules/inventory/employee-supplier/repository.ts
+++ src/modules/inventory/employee-supplier/repository.ts
@@ -1,57 +1,41 @@
-import db from '../../../lib/db'
 import { EmployeeSupplierSchema } from './schema'
+import db from '../../../lib/db'
 
 export default class EmployeeSupplierRepository {
   create = async (payload: EmployeeSupplierSchema) => {
-    try {
-      await db.supplierEmployee.create({ data: payload })
-    } catch (error) {
-      throw error
-    }
+    return await db.supplierEmployee.create({ data: payload })
   }
   update = async (id: number, payload: EmployeeSupplierSchema) => {
-    try {
-      await this.isExist(id)
-      await db.supplierEmployee.update({ data: payload, where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await this.isExist(id)
+    return await db.supplierEmployee.update({ data: payload, where: { id } })
   }
   delete = async (id: number) => {
-    try {
-      await this.isExist(id)
-      await db.supplierEmployee.delete({ where: { id } })
-    } catch (error) {
-      throw error
-    }
+    await this.isExist(id)
+    return await db.supplierEmployee.delete({ where: { id } })
   }
   read = async (id?: number, name?: string) => {
-    try {
-      const baseQuery = {
-        where: {},
-      }
-
-      if (Number(id)) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [{ supplierId: Number(id) }],
-        }
-      }
-      if (name) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [
-            { name: { contains: name.toLowerCase() } },
-            { name: { contains: name.toUpperCase() } },
-            { name: { contains: name } },
-          ],
-        }
-      }
-
-      return await db.supplierEmployee.findMany(baseQuery)
-    } catch (error) {
-      throw error
+    const baseQuery = {
+      where: {},
     }
+
+    if (Number(id)) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [{ supplierId: Number(id) }],
+      }
+    }
+    if (name) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [
+          { name: { contains: name.toLowerCase() } },
+          { name: { contains: name.toUpperCase() } },
+          { name: { contains: name } },
+        ],
+      }
+    }
+
+    return await db.supplierEmployee.findMany(baseQuery)
   }
   isExist = async (id: number) => {
     const data = await db.supplierEmployee.findUnique({ where: { id } })
