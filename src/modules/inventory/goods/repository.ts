6f828f063d550efd7a85ab0afc4954c485497eaa
mmulyana <mulyan.t20@gmail.com>
2024-11-02 21:43:37 +0700

    feat: update

--- src/modules/inventory/goods/repository.ts
+++ src/modules/inventory/goods/repository.ts
@@ -1,117 +1,121 @@
-import db from '../../../lib/db'
 import { deleteFile } from '../../../utils/file'
+import db from '../../../lib/db'
 import { Goods } from './schema'
 
 export default class GoodsRepository {
   create = async (payload: Goods & { photoUrl?: string }) => {
-    try {
-      await db.goods.create({
-        data: {
-          name: payload.name,
-          qty: Number(payload.qty),
-          available: Number(payload.qty),
-          minimum: Number(payload.minimum),
-          photoUrl: payload.photoUrl,
-          location: {
-            connect: {
-              id: Number(payload.locationId),
-            },
+    await db.goods.create({
+      data: {
+        name: payload.name,
+        qty: Number(payload.qty),
+        available: Number(payload.qty),
+        minimum: Number(payload.minimum),
+        photoUrl: payload.photoUrl,
+        location: {
+          connect: {
+            id: Number(payload.locationId),
           },
-          measurement: { connect: { id: Number(payload.measurementId) } },
-          brand: { connect: { id: Number(payload.brandId) } },
-          category: { connect: { id: Number(payload.categoryId) } },
         },
-      })
-    } catch (error) {
-      throw error
-    }
+        measurement: { connect: { id: Number(payload.measurementId) } },
+        brand: { connect: { id: Number(payload.brandId) } },
+        category: { connect: { id: Number(payload.categoryId) } },
+      },
+    })
   }
   update = async (
     id: number,
     payload: Partial<Goods> & { newPhotoUrl?: string }
   ) => {
-    try {
-      if (payload.newPhotoUrl !== '') {
-        const data = await db.brand.findUnique({ where: { id } })
-        if (data?.photoUrl) {
-          deleteFile(data.photoUrl)
-        }
-      }
-      await db.goods.update({
-        data: {
-          name: payload.name,
-          qty: Number(payload.qty),
-          available: Number(payload.qty),
-          minimum: Number(payload.minimum),
-          ...(payload.newPhotoUrl !== ''
-            ? { photoUrl: payload.newPhotoUrl }
-            : undefined),
-          location: {
-            connect: {
-              id: Number(payload.locationId),
-            },
-          },
-          ...(payload.measurementId !== ''
-            ? {
-                measurement: { connect: { id: Number(payload.measurementId) } },
-              }
-            : undefined),
-          ...(payload.brandId !== ''
-            ? {
-                brand: { connect: { id: Number(payload.brandId) } },
-              }
-            : undefined),
-          ...(payload.categoryId !== ''
-            ? {
-                category: { connect: { id: Number(payload.categoryId) } },
-              }
-            : undefined),
-        },
-        where: { id },
-      })
-    } catch (error) {
-      throw error
-    }
-  }
-  delete = async (id: number) => {
-    try {
-      const data = await db.goods.findUnique({ where: { id } })
-
+    if (payload.newPhotoUrl !== '') {
+      const data = await db.brand.findUnique({ where: { id } })
       if (data?.photoUrl) {
         deleteFile(data.photoUrl)
       }
-
-      await db.goods.delete({ where: { id } })
-    } catch (error) {
-      throw error
     }
+    return await db.goods.update({
+      data: {
+        name: payload.name,
+        qty: Number(payload.qty),
+        available: Number(payload.qty),
+        minimum: Number(payload.minimum),
+        ...(payload.newPhotoUrl !== ''
+          ? { photoUrl: payload.newPhotoUrl }
+          : undefined),
+        location: {
+          connect: {
+            id: Number(payload.locationId),
+          },
+        },
+        ...(payload.measurementId !== ''
+          ? {
+              measurement: { connect: { id: Number(payload.measurementId) } },
+            }
+          : undefined),
+        ...(payload.brandId !== ''
+          ? {
+              brand: { connect: { id: Number(payload.brandId) } },
+            }
+          : undefined),
+        ...(payload.categoryId !== ''
+          ? {
+              category: { connect: { id: Number(payload.categoryId) } },
+            }
+          : undefined),
+      },
+      where: { id },
+      select: {
+        id: true,
+      },
+    })
+  }
+  delete = async (id: number) => {
+    const data = await db.goods.findUnique({ where: { id } })
+
+    if (data?.photoUrl) {
+      deleteFile(data.photoUrl)
+    }
+
+    await db.goods.delete({ where: { id } })
   }
   read = async (name: string | undefined) => {
-    try {
-      const baseQuery = {
-        where: {},
-        include: {
-          brand: true,
-          category: true,
-          location: true,
-          measurement: true,
-        },
-      }
-
-      if (name) {
-        baseQuery.where = {
-          ...baseQuery.where,
-          OR: [
-            { name: { contains: name.toLowerCase() } },
-            { name: { contains: name.toUpperCase() } },
-            { name: { contains: name } },
-          ],
-        }
-      }
-
-      return await db.goods.findMany(baseQuery)
-    } catch (error) {
-      throw error
+    const baseQuery = {
+      where: {},
+      include: {
+        brand: true,
+        category: true,
+        location: true,
+        measurement: true,
+      },
     }
+
+    if (name) {
+      baseQuery.where = {
+        ...baseQuery.where,
+        OR: [
+          { name: { contains: name.toLowerCase() } },
+          { name: { contains: name.toUpperCase() } },
+          { name: { contains: name } },
+        ],
+      }
+    }
+
+    return await db.goods.findMany(baseQuery)
+  }
+  readOne = async (id: number) => {
+    return await db.goods.findUnique({
+      where: { id },
+      include: {
+        brand: true,
+        category: true,
+        location: true,
+        measurement: true,
+        transaction: true,
+        _count: {
+          select: {
+            transaction: true,
+          },
+        },
+      },
+    })
   }
 }
