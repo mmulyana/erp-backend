7a12ec6dd0022d32df9af6005dab4db94c5fc3c8
mmulyana <mulyan.t20@gmail.com>
2024-11-02 10:49:24 +0700

    fix: schema

--- src/modules/inventory/transaction/schema.ts
+++ src/modules/inventory/transaction/schema.ts
@@ -1,14 +1,17 @@
 import { z } from 'zod'
 
-export const schema = z.object({
+const transactionItemSchema = z.object({
   goodsId: z.string(),
   qty: z.string(),
   price: z.string().optional(),
-  supplierId: z.string().optional(),
-  date: z.string(),
   type: z.enum(['in', 'out', 'opname', 'borrowed', 'returned']),
-  description: z.string().optional(),
-  projectId: z.string().optional(),
-  isReturned: z.string().optional(),
 })
+
+export const schema = z.object({
+  date: z.string(),
+  items: z.array(transactionItemSchema),
+  supplierId: z.string().optional(),
+})
+
+export type TransactionItem = z.infer<typeof transactionItemSchema>
 export type Transaction = z.infer<typeof schema>
