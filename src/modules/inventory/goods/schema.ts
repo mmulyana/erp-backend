c3bda3dee0d5b18d620b70012e73e1ddb381111d
mmulyana <mulyan.t20@gmail.com>
2024-11-04 05:56:28 +0700

    feat: update

--- src/modules/inventory/goods/schema.ts
+++ src/modules/inventory/goods/schema.ts
@@ -2,13 +2,13 @@ import { z } from 'zod'
 
 export const goodsSchema = z.object({
   name: z.string(),
-  minimum: z.string(),
+  minimum: z.string().optional(),
   qty: z.string(),
-  available: z.string(),
-  locationId: z.string(),
-  measurementId: z.string(),
-  categoryId: z.string(),
-  brandId: z.string(),
+  available: z.string().optional(),
+  locationId: z.string().optional(),
+  measurementId: z.string().optional(),
+  categoryId: z.string().optional(),
+  brandId: z.string().optional(),
 })
 export const updateGoodsSchema = goodsSchema.partial()
 export type Goods = z.infer<typeof goodsSchema>
