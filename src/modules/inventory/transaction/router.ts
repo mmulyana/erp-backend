b041de4f53f2a3e57edca75ab66b57e3997b7edd
mmulyana <mulyan.t20@gmail.com>
2024-11-03 23:23:05 +0700

    feat: update

--- src/modules/inventory/transaction/router.ts
+++ src/modules/inventory/transaction/router.ts
@@ -1,8 +1,8 @@
-import { Multer } from 'multer'
 import RouterWithFile from '../../../helper/router-with-file'
 import Validation from '../../../helper/validation'
 import Controller from './controller'
 import { schema } from './schema'
+import { Multer } from 'multer'
 
 export default class TransactionRouter extends RouterWithFile {
   private schema: Validation = new Validation(schema)
@@ -18,6 +18,7 @@ export default class TransactionRouter extends RouterWithFile {
     this.router.patch('/:id', this.controller.updateHandler)
     this.router.delete('/:id', this.controller.deleteHandler)
     this.router.get('/', this.controller.readHandler)
+    this.router.get('/:id', this.controller.readOneHandler)
     this.router.get('/data/borrowed', this.controller.readBorrowedGoodsHandler)
   }
 }
