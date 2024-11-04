7b4259e3399251b360728f6ce522b5357d7e58f6
mmulyana <mulyan.t20@gmail.com>
2024-11-04 17:01:48 +0700

    feat: update

--- src/modules/inventory/transaction/router.ts
+++ src/modules/inventory/transaction/router.ts
@@ -1,13 +1,13 @@
 import RouterWithFile from '../../../helper/router-with-file'
 import Validation from '../../../helper/validation'
-import Controller from './controller'
 import { schema, updateSchema } from './schema'
+import Controller from './controller'
 import { Multer } from 'multer'
 
 export default class TransactionRouter extends RouterWithFile {
   private create: Validation = new Validation(schema)
   private update: Validation = new Validation(updateSchema)
-  
+
   private controller: Controller = new Controller()
 
   constructor(upload: Multer) {
@@ -17,10 +17,15 @@ export default class TransactionRouter extends RouterWithFile {
 
   protected register(): void {
     this.router.post('/', this.create.validate, this.controller.createHandler)
-    this.router.patch('/:id', this.update.validate, this.controller.updateHandler)
-    this.router.delete('/:id', this.controller.deleteHandler)
+    this.router.patch(
+      '/:id',
+      this.update.validate,
+      this.controller.updateHandler
+    )
     this.router.get('/', this.controller.readHandler)
     this.router.get('/:id', this.controller.readOneHandler)
-    this.router.get('/data/borrowed', this.controller.readBorrowedGoodsHandler)
+    this.router.delete('/:id', this.controller.deleteHandler)
+    this.router.get('/list/pagination', this.controller.readByPaginationHandler)
+    this.router.get('/list/borrowed', this.controller.readBorrowedGoodsHandler)
   }
 }
