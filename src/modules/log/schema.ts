4e28823a804166d3d3255b0716cd480e17fb58b1
mmulyana <mulyan.t20@gmail.com>
2024-11-05 07:55:03 +0700

    feat: remove module role permission

--- src/modules/log/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const updateSchema = z.object({
-  userId: z.number(),
-  action: z.string(),
-  module: z.string(),
-  description: z.string(),
-  timestamp: z.string().datetime(),
-})
