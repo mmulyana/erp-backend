d93c47f3e26819c438c095547b2be51af7ede35e
mmulyana <mulyan.t20@gmail.com>
2024-11-07 03:41:32 +0700

    feat: update

--- src/modules/account/schema.ts
+++ src/modules/account/schema.ts
@@ -1,16 +1,44 @@
-import { z } from 'zod'
+import { string, z } from 'zod'
 
-export const AccountSchema = {
-  create: z.object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string().optional(),
-    rolesId: z.number().optional(),
-  }),
-  update: z.object({
-    name: z.string().optional(),
-    email: z.string().optional(),
-    password: z.string().optional(),
-    rolesId: z.number().optional(),
-  }),
+const phoneNumberRegex =
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+const passwordLength = 8
+
+const baseSchema = {
+  name: z
+    .string()
+    .min(1, 'Nama harus lebih dari 1 huruf')
+    .nullable()
+    .optional(),
+  email: z.string().email('Format email salah').nullable().optional(),
+  phoneNumber: z
+    .string()
+    .regex(phoneNumberRegex, 'Format nomor telepon salah')
+    .nullable()
+    .optional(),
+  employeeId: z.number().int().positive().nullable().optional(),
 }
+
+export const createAccountSchema = z
+  .object({
+    ...baseSchema,
+    password: z
+      .string()
+      .min(passwordLength, `Password minimal ${passwordLength} karakter`),
+    permissions: string().array(),
+  })
+  .strict()
+
+export const updateAccountSchema = z
+  .object({
+    ...baseSchema,
+    password: z
+      .string()
+      .min(passwordLength, `Password minimal ${passwordLength} karakter`)
+      .optional(),
+  })
+  .partial()
+  .strict()
+
+export type CreateAccountSchema = z.infer<typeof createAccountSchema>
+export type UpdateAccountSchema = z.infer<typeof updateAccountSchema>
