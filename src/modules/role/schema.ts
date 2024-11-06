5a29b8fca12bc641a3f0ba9aa8d6c615bb789dbe
mmulyana <mulyan.t20@gmail.com>
2024-11-07 05:09:03 +0700

    feat: update

--- src/modules/role/schema.ts
+++ src/modules/role/schema.ts
@@ -6,3 +6,6 @@ export const createRoleSchema = z.object({
   permissionIds: z.number().array(),
 })
 export const updateRoleSchema = createRoleSchema.partial()
+
+export type createRoleDTO = z.infer<typeof createRoleSchema>
+export type updateRoleDTO = z.infer<typeof updateRoleSchema>
