72e67e2a8c2d82a444e1db9113b13fdc90906438
mmulyana <mulyan.t20@gmail.com>
2024-11-07 05:08:57 +0700

    feat: update

---
+++ src/modules/role/controller.ts
@@ -0,0 +1,111 @@
+import { NextFunction, Request, Response } from 'express'
+import BaseController from '../../helper/base-controller'
+import RoleService from './service'
+
+export default class RoleController extends BaseController {
+  private service: RoleService = new RoleService()
+
+  constructor() {
+    super('role')
+  }
+
+  getRolesHandler = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await this.service.getRoles()
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  getRoleByIdHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.service.getRoleBydId(Number(id))
+      return this.response.success(res, this.message.successRead(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  createRoleHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      await this.service.createRole(req.body)
+      return this.response.success(res, this.message.successCreate())
+    } catch (error) {
+      next(error)
+    }
+  }
+  updateRoleHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.service.updateRole(Number(id), req.body)
+      return this.response.success(res, this.message.successUpdate(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  deleteRoleHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { id } = req.params
+      const data = await this.service.deleteRole(Number(id))
+      return this.response.success(res, this.message.successDelete(), data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  addPermissionRoleHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { roleId, permissionId } = req.query
+      const data = await this.service.addPermissionRole(
+        Number(roleId),
+        Number(permissionId)
+      )
+      return this.response.success(
+        res,
+        this.message.successCreateField('hak istimewa'),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+  deletePermissionRoleHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { roleId, permissionId } = req.query
+      const data = await this.service.removePermissionRole(
+        Number(roleId),
+        Number(permissionId)
+      )
+      return this.response.success(
+        res,
+        this.message.successDeleteField('hak istimewa'),
+        data
+      )
+    } catch (error) {
+      next(error)
+    }
+  }
+}
