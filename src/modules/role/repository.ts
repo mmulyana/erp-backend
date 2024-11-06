2ee29512a7a1623aa3178da877bedf0e69f99bdc
mmulyana <mulyan.t20@gmail.com>
2024-11-07 05:02:17 +0700

    feat: update

---
+++ src/modules/role/repository.ts
@@ -0,0 +1,66 @@
+import { Prisma } from '@prisma/client'
+import db from '../../lib/db'
+
+export default class RoleRepository {
+  getAll = async () => {
+    return await db.role.findMany({
+      include: {
+        rolePermissions: {
+          include: {
+            permission: true,
+          },
+        },
+        _count: {
+          select: {
+            users: true,
+          },
+        },
+      },
+    })
+  }
+  getById = async (id: number) => {
+    return await db.role.findUnique({
+      where: { id },
+      include: {
+        rolePermissions: {
+          include: {
+            permission: true,
+          },
+        },
+        _count: {
+          select: {
+            users: true,
+          },
+        },
+      },
+    })
+  }
+  updateById = async (id: number, data: Prisma.RoleUpdateInput) => {
+    return await db.role.update({ where: { id }, data })
+  }
+  create = async (data: Prisma.RoleCreateInput) => {
+    return await db.role.create({ data })
+  }
+  deleteById = async (id: number) => {
+    return await db.role.delete({ where: { id } })
+  }
+  getPermissionById = async (id: number) => {
+    return await db.permission.findUnique({ where: { id } })
+  }
+  createPermissionRole = async (data: {
+    roleId: number
+    permissionId: number
+  }) => {
+    return await db.rolePermission.create({ data })
+  }
+  deletePermissionRole = async (data: {
+    roleId: number
+    permissionId: number
+  }) => {
+    return await db.rolePermission.delete({
+      where: {
+        roleId_permissionId: data,
+      },
+    })
+  }
+}
