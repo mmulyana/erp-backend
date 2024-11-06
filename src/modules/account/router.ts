15ae8a8c8a571300a163505c93f0c9eec0f8ee6b
mmulyana <mulyan.t20@gmail.com>
2024-11-07 03:42:09 +0700

    feat: update

--- src/modules/account/router.ts
+++ src/modules/account/router.ts
@@ -1,24 +1,49 @@
-import { Router } from 'express'
-import { AccountSchema } from './schema'
-import AccountController from './controller'
+import RouterWithFile from '../../helper/router-with-file'
+import { MulterConfig } from '../../utils/multer-config'
 import Validation from '../../helper/validation'
+import AccountController from './controller'
+import { createAccountSchema, updateAccountSchema } from './schema'
 
-export default class AccountRouter {
-  public router: Router
+export default class AccountRouter extends RouterWithFile {
   private controller: AccountController = new AccountController()
-  private createValidation: Validation = new Validation(AccountSchema.create)
-  private updateValidation: Validation = new Validation(AccountSchema.update)
+  private create: Validation = new Validation(createAccountSchema)
+  private update: Validation = new Validation(updateAccountSchema)
 
-  constructor() {
-    this.router = Router()
-    this.register()
+  constructor(multer: MulterConfig) {
+    super(multer.uploadImg, 'Akun')
   }
 
   protected register() {
-    this.router.get('/', this.controller.getAccounts)
-    this.router.get('/:id', this.controller.getAccount)
-    this.router.delete('/:id', this.controller.deleteAccount)
-    this.router.post('/', this.createValidation.validate, this.controller.createAccount)
-    this.router.patch('/:id', this.updateValidation.validate, this.controller.updateAccount)
+    this.router.get(
+      '/:id',
+      this.create.validate,
+      this.controller.getAccountHandler
+    )
+    this.router.delete('/:id', this.controller.deleteAccountHandler)
+    this.router.patch(
+      '/:id',
+      this.upload.single('photo'),
+      this.compressImage,
+      this.update.validate,
+      this.controller.updateAccountHandler
+    )
+    this.router.post(
+      '/',
+      this.upload.single('photo'),
+      this.create.validate,
+      this.controller.createAccountHandler
+    )
+    this.router.patch(
+      '/:id/role/add/:roleId',
+      this.controller.updateRoleAccountHandler
+    )
+    this.router.patch(
+      '/:id/permission/add/:permissionId',
+      this.controller.createPermissionHandler
+    )
+    this.router.patch(
+      '/:id/permission/remove/:permissionId',
+      this.controller.createPermissionHandler
+    )
   }
 }
