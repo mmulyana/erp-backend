6386a324708df5d6c0ee23d7b0504e51c8f75f3e
mmulyana <mulyan.t20@gmail.com>
2024-11-07 21:19:00 +0700

    feat: update

--- src/modules/project/board/router.ts
+++ src/modules/project/board/router.ts
@@ -14,9 +14,19 @@ export default class CompanyRouter {
   }
 
   protected register() {
-    this.router.post('/', this.boardSchema.validate, this.controller.handleCreate)
-    this.router.patch('/:id', this.boardSchema.validate, this.controller.handleUpdate)
+    this.router.post(
+      '/',
+      this.boardSchema.validate,
+      this.controller.handleCreate
+    )
+    this.router.patch(
+      '/:id',
+      this.boardSchema.validate,
+      this.controller.handleUpdate
+    )
     this.router.delete('/:id', this.controller.handleDelete)
     this.router.get('/', this.controller.handleRead)
+    
+    this.router.get('/data/chart', this.controller.handleBoardChart)
   }
 }
