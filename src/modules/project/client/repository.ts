1106caf456d0643982b96f03092943170a500da7
mmulyana <mulyan.t20@gmail.com>
2024-11-07 21:16:13 +0700

    feat: update

--- src/modules/project/client/repository.ts
+++ src/modules/project/client/repository.ts
@@ -1,6 +1,19 @@
 import db from '../../../lib/db'
 import { Client } from './schema'
 
+type ChartData = {
+  client: string
+  count: number
+  fill?: string | null
+}
+
+type ChartConfig = {
+  [key: string]: {
+    label: string
+    color: string
+  }
+}
+
 export default class ClientRepository {
   create = async (payload: Client) => {
     await db.client.create({
@@ -31,5 +44,56 @@ export default class ClientRepository {
         company: true,
       },
     })
+  }
+  getTopClients = async () => {
+    const data = await db.client.findMany({
+      select: {
+        id: true,
+        name: true,
+        position: true,
+        _count: {
+          select: {
+            Project: true,
+          },
+        },
+      },
+      orderBy: {
+        Project: {
+          _count: 'desc',
+        },
+      },
+      take: 5,
+    })
+
+    const clients = data.map((item) => ({
+      clientId: item.id,
+      clientName: item.name,
+      count: item._count.Project,
+    }))
+
+    const colors = ['#2A9D90', '#3B82F6', '#10B981', '#F59E0B', '#EF4444']
+
+    const chartData: ChartData[] = clients.map((item, index) => ({
+      client: item.clientName.toLowerCase(),
+      count: item.count,
+      fill: colors[index],
+    }))
+
+    const chartConfig: ChartConfig = clients.reduce<ChartConfig>(
+      (config, item, index) => {
+        const clientKey = item.clientName.toLowerCase()
+        config[clientKey] = {
+          label: item.clientName,
+          color: colors[index],
+        }
+        return config
+      },
+      {}
+    )
+
+    return {
+      chartData,
+      chartConfig,
+    }
   }
 }
