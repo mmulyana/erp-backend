70f8e25f50edc7cc7344b8b267b4723f4ea8913a
mmulyana <mulyan.t20@gmail.com>
2024-11-07 21:18:33 +0700

    feat: update

--- src/modules/project/board/repository.ts
+++ src/modules/project/board/repository.ts
@@ -2,6 +2,17 @@ import { generateUUID } from '../../../utils/generate-uuid'
 import { Board } from './schema'
 import db from '../../../lib/db'
 
+type ChartData = {
+  [key: string]: number
+}
+
+type ChartConfig = {
+  [key: string]: {
+    label: string
+    color: string
+  }
+}
+
 export default class BoardRepository {
   create = async (data: Board) => {
     const lastContainer = await db.boardContainer.findFirst({
@@ -25,5 +36,50 @@ export default class BoardRepository {
         position: 'asc',
       },
     })
+  }
+  getBoardChart = async () => {
+    const boardStats = await db.boardContainer.findMany({
+      select: {
+        id: true,
+        name: true,
+        color: true,
+        _count: {
+          select: {
+            items: true,
+          },
+        },
+      },
+      orderBy: {
+        position: 'asc',
+      },
+    })
+
+    const chartData: ChartData[] = [
+      {
+        ...boardStats.reduce(
+          (acc, board) => ({
+            ...acc,
+            [board.name]: board._count.items,
+          }),
+          {}
+        ),
+      },
+    ]
+
+    const chartConfig: ChartConfig = boardStats.reduce((config, board) => {
+      const key = board.name
+
+      config[key] = {
+        label: board.name,
+        color: board.color,
+      }
+
+      return config
+    }, {} as ChartConfig)
+
+    return {
+      chartData,
+      chartConfig,
+    }
   }
 }
