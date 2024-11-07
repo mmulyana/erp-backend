100d07cb2d8e51e22888c7c631d1062492097b62
mmulyana <mulyan.t20@gmail.com>
2024-11-07 13:00:48 +0700

    feat: update

--- src/modules/hris/cash-advance/schema.ts
+++ src/modules/hris/cash-advance/schema.ts
@@ -4,5 +4,5 @@ export const cashAdvanceSchema = z.object({
   employeeId: z.number(),
   amount: z.number(),
   requestDate: z.string().date(),
-  description: z.string().optional(),
+  description: z.string().optional().nullable(),
 })
