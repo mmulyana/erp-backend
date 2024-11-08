b8e485e5311b68318780c5e720eff9729a1491db
mmulyana <mulyan.t20@gmail.com>
2024-11-08 20:00:39 +0700

    feat: update

--- src/modules/project/index/schema.ts
+++ src/modules/project/index/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const createProjectSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(1, "Nama proyek tidak boleh kosong"),
   containerId: z.string(),
   progress: z.number().optional().default(0),
   payment_status: z.number().optional().default(0),
