51beb549beb31422303fc91f18f3a6be55fbb851
mmulyana <mulyan.t20@gmail.com>
2025-05-22 01:02:14 +0700

    feat: update

--- src/modules/hris/payroll-period/router.ts
+++ src/modules/hris/payroll-period/router.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 import {
   getPayrollPeriod,
   getPayrollPeriods,
+  getPayrollPeriodsInfinite,
   postPayrollPeriod,
 } from './controller'
 
 const router = Router()
+
+router.get('/data/infinite', getPayrollPeriodsInfinite)
 
 router.get('/', getPayrollPeriods)
 router.post('/', postPayrollPeriod)
