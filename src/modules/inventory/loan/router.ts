6be9650cc4b8461c43418498cf000fa730cd26e9
mmulyana <mulyan.t20@gmail.com>
2025-05-25 00:57:27 +0700

    feat: update

--- src/modules/inventory/loan/router.ts
+++ src/modules/inventory/loan/router.ts
@@ -1,12 +1,20 @@
 import upload from '@/utils/upload'
 import { Router } from 'express'
-import { getLoans, getStatusByMonth, postLoan } from './controller'
+import {
+  getLoan,
+  getLoans,
+  getStatusByMonth,
+  patchLoan,
+  postLoan,
+} from './controller'
 
 const router = Router()
 
 router.get('/data/status', getStatusByMonth)
 
 router.get('/', getLoans)
-router.post('/', upload.array('photoUrl', 10), postLoan)
+router.get('/:id', getLoan)
+router.post('/', upload.single('photoUrl'), postLoan)
+router.patch('/:id', upload.single('photoUrlIn'), patchLoan)
 
 export default router
