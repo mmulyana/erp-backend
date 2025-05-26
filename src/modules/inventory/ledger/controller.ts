69885d87a35b483975bc773cc4215fd568018293
mmulyana <mulyan.t20@gmail.com>
2025-05-27 02:15:08 +0700

    feat: update

--- src/modules/inventory/ledger/controller.ts
+++ src/modules/inventory/ledger/controller.ts
@@ -1,38 +1,51 @@
 import { Request, Response } from 'express'
-import { readChart, readTable } from './repository'
+
 import { successResponse } from '@/utils/response'
 import { getParams } from '@/utils/params'
+import { getQueryParam } from '@/utils'
+
+import { readAll, readChart, readTable } from './repository'
 
 export const getChart = async (req: Request, res: Response) => {
-  const startDate = req.query.startDate
-    ? new Date(req.query.startDate as string)
-    : undefined
-  const endDate = req.query.endDate
-    ? new Date(req.query.endDate as string)
-    : undefined
+  const startDate = getQueryParam(req.query, 'startDate', 'string')
+  const endDate = getQueryParam(req.query, 'endDate', 'string')
 
   const result = await readChart({
-    endDate,
-    startDate,
+    startDate: new Date(startDate),
+    endDate: new Date(endDate),
   })
   res.json(successResponse(result, 'Bagan transaksi'))
 }
 
 export const getTable = async (req: Request, res: Response) => {
   const { page, limit, search } = getParams(req)
-  const startDate = req.query.startDate
-    ? new Date(req.query.startDate as string)
-    : undefined
-  const endDate = req.query.endDate
-    ? new Date(req.query.endDate as string)
-    : undefined
+  const startDate = getQueryParam(req.query, 'startDate', 'string')
+  const endDate = getQueryParam(req.query, 'endDate', 'string')
 
   const result = await readTable({
-    endDate,
-    startDate,
+    startDate: new Date(startDate),
+    endDate: new Date(endDate),
     limit,
     page,
     search,
   })
   res.json(successResponse(result, 'Tabel transaksi'))
+}
+
+export const getLedgers = async (req: Request, res: Response) => {
+  const { page, limit, search, sortBy, sortOrder } = getParams(req)
+  const itemId = getQueryParam(req.query, 'itemId', 'string')
+  const type = getQueryParam(req.query, 'type', 'string') as any
+
+  const result = await readAll({
+    page,
+    limit,
+    search,
+    itemId,
+    sortBy,
+    sortOrder,
+    type,
+  })
+
+  res.json(successResponse(result, 'ledger'))
 }
